2b8348cc6236a3953321edfcfc2ef6ae4e6b8b86
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-23 18:05:55 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,8 +1,9 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, RE AS '중성화'
+SELECT ANIMAL_ID, NAME, 중성화
     FROM (SELECT *,
-                CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-                     ELSE 'X' END AS 'RE'
+                CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
+                        OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+                     ELSE 'X' END AS 중성화
           FROM ANIMAL_INS
           )AS A
     ORDER
