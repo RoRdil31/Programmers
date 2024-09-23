a2d47f960110b21dfe7b2c96fbfb86ef9b73e9c3
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-23 18:11:36 +0900

    Update 중성화 여부 파악하기.sql

--- MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,10 +1,33 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, 중성화
-    FROM (SELECT *,
-                CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
-                        OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-                     ELSE 'X' END AS 중성화
-          FROM ANIMAL_INS
-          )AS A
-    ORDER
-       BY ANIMAL_ID
+# SELECT ANIMAL_ID, NAME, 중성화
+#     FROM (SELECT *,
+#                 CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
+#                         OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+#                      ELSE 'X' END AS 중성화
+#           FROM ANIMAL_INS
+#           )AS A
+#     ORDER
+#        BY ANIMAL_ID
+
+
+
+-- LIKE 사용. 더 간단화
+# SELECT ANIMAL_ID, NAME,
+#         CASE
+#             WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
+#                 OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+#             ELSE 'X'
+#         END 중성화
+#     FROM ANIMAL_INS
+#     ORDER BY ANIMAL_ID
+    
+    
+    
+-- REGEXP 정규식 사용
+
+SELECT ANIMAL_ID, NAME,
+        CASE
+            WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
+            ELSE 'X'
+        END 중성화
+    FROM ANIMAL_INS
+    ORDER BY ANIMAL_ID
