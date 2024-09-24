dc76cb31d0a527e729c9a3e578dd1b58c148d50d
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-24 21:36:28 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -0,0 +1,10 @@
+SELECT ID, LENGTH
+    FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY LENGTH DESC, ID) AS 순위
+          FROM FISH_INFO 
+    
+    ) FISH_INFO
+    WHERE 순위 <= 10;
+
+# SELECT *, ROW_NUMBER() OVER (ORDER BY LENGTH DESC, ID) AS 순위
+#           FROM FISH_INFO 
+          
