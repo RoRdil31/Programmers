09061932f15e82dd6028123444761c7d22fc7c40
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-28 14:53:24 +0900

    sqrt로 for문 연산, list comprehension 연산

--- Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,16 +1,17 @@
-# import math
-# def solution(n):
-#     sum=0
-#     for i in range(1,n+1):
-#         sum += i if n%i == 0 else 0
-#     return sum
+# Basic for문
+def solution(n):
+    sum=0
+    for i in range(1,n+1):
+        sum += i if n%i == 0 else 0
+    return sum
 
+# list comprehension
 def solution(n):
     return sum([i for i in range(1,n+1) if n%i ==0])
 
-# import math
-# def solution(n):
-#     sum=0
-#     for i in range(1,math.ceil(math.sqrt(n))):
-#         sum += (i+n//i)if n%i == 0 else 0
-#     return sum
+# sqrt로 for문 연산 줄이는 방법
+import math
+def solution(n):
+    l1 = [i for i in range(1,int(math.sqrt(n))+1) if n%i == 0]
+    l1.extend([n//i for i in l1])
+    return sum(set(l1))
