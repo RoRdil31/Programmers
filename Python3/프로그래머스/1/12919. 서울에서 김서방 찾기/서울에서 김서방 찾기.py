f554b546f853c333e68df7f043ff048af75c877b
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-29 16:18:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ Python3/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,3 +1,6 @@
+# def solution(seoul):
+#     here = seoul.index('Kim')
+#     return f'김서방은 {here}에 있다'
+
 def solution(seoul):
-    here = seoul.index('Kim')
-    return f'김서방은 {here}에 있다'
+    return '김서방은 {}에 있다'.format(seoul.index('Kim'))
