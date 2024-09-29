0d3b30e53e72408f9cb6de72d49d6cfbf9c7c30a
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-09-29 15:26:24 +0900

    list ( reversed ( list ) )

--- Python3/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ Python3/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,2 +1,5 @@
+# def solution(n):
+#     return list(reversed([int(i) for i in list(str(n))]))
+
 def solution(n):
-    return list(reversed([int(i) for i in list(str(n))]))
+    return [int(i) for i in reversed(str(n))]
