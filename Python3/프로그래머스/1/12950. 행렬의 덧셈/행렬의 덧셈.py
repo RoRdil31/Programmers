b7c8de444d5d85feb464a0138059c02b1e23e513
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-03 21:10:35 +0900

    numpy 사용 코드, 이중 for문 사용 코드

--- Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -2,3 +2,15 @@ import numpy as np
 def solution(arr1, arr2):
     answer = np.array(arr1)+np.array(arr2)
     return answer.tolist()
+
+
+def solution(arr1, arr2):
+    answer = []
+    
+    for i,j in zip(arr1, arr2):
+        sum = []
+        for n,m in zip(i,j):
+            sum.append(n+m)
+        answer.append(sum)
+    
+    return answer
