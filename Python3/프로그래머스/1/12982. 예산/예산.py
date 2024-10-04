9735193448a9c3effc4c7b573ec9e008440e1911
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-04 15:12:53 +0900

    [level 1] Title: 예산, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12982. 예산/예산.py
+++ Python3/프로그래머스/1/12982. 예산/예산.py
@@ -1,8 +1,16 @@
-def solution(d, budget):
-    cnt = 0
-    for i in sorted(d):
-        if budget - i < 0 : return cnt
-        cnt += 1
-        budget -= i
+# def solution(d, budget):
+#     cnt = 0
+#     for i in sorted(d):
+#         if budget - i < 0 : return cnt
+#         cnt += 1
+#         budget -= i
     
-    return cnt
+#     return cnt
+
+def solution(d, budget):
+    
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    
+    return len(d)
