7779244fd885f9dd66b57c77903dae41d8a95119
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-04 15:29:46 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,13 +1,12 @@
 def solution(n):
-    answer = 0
     ternary = ''
     while n > 0:
         ternary += str(n%3)
         n = n//3
     
-    up = len(ternary)-1
-    for i in ternary:
-        answer += int(i)*(3**up)
-        up -= 1
+    # up = len(ternary)-1
+    # for i in ternary:
+    #     answer += int(i)*(3**up)
+    #     up -= 1
         
-    return answer
+    return int(ternary,3)
