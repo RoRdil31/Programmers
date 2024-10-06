5dc8f04d78eec0ba23c3d69b59a2d09dd256040f
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-06 15:51:01 +0900

    [level 1] Title: 최소직사각형, Time: 3.42 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ Python3/프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,10 @@
-def solution(sizes):
-    row_max, col_max = 0,0
-    for s in sizes:
-        row_max = max(max(s[0],s[1]),row_max)# if max(s[0],s[1]) > row_max else row_max
-        col_max = max(min(s[0],s[1]),col_max)# if min(s[0],s[1]) > col_max else col_max
+# def solution(sizes):
+#     row_max, col_max = 0,0
+#     for s in sizes:
+#         row_max = max(max(s[0],s[1]),row_max)
+#         col_max = max(min(s[0],s[1]),col_max)
         
-    return row_max * col_max
+#     return row_max * col_max
+
+def solution(sizes):
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
