d7674b456954e9b43c19f1a0d70b795cd61460dd
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-12 11:13:02 +0900

    [level 1] Title: 과일 장수, Time: 90.26 ms, Memory: 29.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,11 @@
+# def solution(k, m, score):
+#     price = 0
+#     score = sorted(score,reverse=True)
+    
+#     for i in range(0,len(score)//m*m,m):
+#         price += m * min(score[i:i+m])
+    
+#     return price
+
+def solution(k, m, score):
+    return sum(sorted(score)[len(score)%m::m])*m
