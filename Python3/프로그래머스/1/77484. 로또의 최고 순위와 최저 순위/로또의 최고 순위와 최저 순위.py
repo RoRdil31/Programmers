46cb1404ef0843319d01ec0510e42d80f9a2f218
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-13 14:37:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,11 +1,21 @@
 def solution(lottos, win_nums):
-    score = 0
-    hidden_score = 0
+    rank = [6,6,5,4,3,2,1]
+    cnt = 0
+    hidden_cnt = lottos.count(0)
     
-    for l in lottos:
-        if l==0 : hidden_score += 1
-        elif l in win_nums : 
-            score += 1
-            win_nums.remove(l)
-    if score+hidden_score == 0 : hidden_score = 1
-    return [7-score-hidden_score, (7-score) if score!=0 else 6]
+    for i in lottos:
+        if i in win_nums: cnt += 1
+    
+    return [rank[cnt + hidden_cnt],rank[cnt]]
+#     score = 0
+#     hidden_score = 0
+    
+#     for l in lottos:
+#         if l==0 : hidden_score += 1
+#         elif l in win_nums : 
+#             score += 1
+#             win_nums.remove(l)
+            
+#     if score+hidden_score == 0 : hidden_score = 1
+    
+#     return [7-score-hidden_score, (7-score) if score!=0 else 6]
