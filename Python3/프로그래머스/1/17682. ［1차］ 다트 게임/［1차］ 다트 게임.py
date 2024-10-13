7115e6c44404830bbc2329e33dcef95739ed9c8e
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-13 13:25:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.24 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ Python3/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,24 +1,38 @@
+# import re
+
+# def SDT(num, c):
+#     if c == 'S' : return num
+#     elif c == 'D' : return num ** 2
+#     else : return num ** 3
+    
+# def solution(dartResult):
+#     answer = []
+#     dart = re.findall(r'\d+|[a-zA-Z][^\d\s]*',dartResult) # 숫자 또는 문자+특수문자 조합 찾음
+    
+#     for idx, chars in zip(range(1,len(dart)+1,2),dart[1::2]):
+#         answer.append(SDT(int(dart[idx-1]),chars[0])) # 숫자,(Single/Double/Triple 연산)
+#         if len(chars) == 2 : 
+#             if chars[1] == '*':
+#                 answer[len(answer)-1] *= 2
+#                 if len(answer) != 1 : answer[len(answer)-2] *= 2
+#             elif chars[1] == '#':
+#                 answer[len(answer)-1] *= (-1)
+    
+#     return sum(answer)
+
 import re
 
-def SDT(num, c):
-    if c == 'S' : return num
-    elif c == 'D' : return num ** 2
-    else : return num ** 3
-    
 def solution(dartResult):
-    answer = []
-    dart = re.findall(r'\d+|[a-zA-Z][^\d\s]*',dartResult) # 숫자 또는 문자+특수문자 조합 찾음
-    for idx, chars in zip(range(1,len(dart)+1,2),dart[1::2]):
-        answer.append(SDT(int(dart[idx-1]),chars[0])) # 숫자,(Single/Double/Triple 연산)
-        if len(chars) == 2 : 
-            if chars[1] == '*':
-                answer[len(answer)-1] *= 2
-                if len(answer) != 1 : answer[len(answer)-2] *= 2
-            elif chars[1] == '#':
-                answer[len(answer)-1] *= (-1)
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    # print(dart) 
     
-    return sum(answer)
-
-# 스타상 (*) => 해당 점수, 직전 점수 2배로 만듦. 
-    #           (첫번째로 나오면 그 것만 2배) (스타/아차 중첩가능)
-# 아차상 (#) => 해당 점수 감점.
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+    # print(dart)
+    answer = sum(dart)
+    return answer
