dd85a5c7f4cac614a50744e77b2fa5eba461edb5
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-17 11:07:47 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,14 @@
+def solution(n, lost, reserve):
+    
+    students = [1]*(n+2)
+    for i in lost : 
+        if i in reserve : reserve.remove(i);continue
+        students[i] = 0
+        
+    # for i in reserve : 
+    #     if students[i] == 0 : students[i] = 1 ;reserve.remove(i)
+    for i in sorted(reserve) :
+        if  students[i-1] == 0 : students[i-1] = 1
+        elif students[i+1] == 0 : students[i+1] = 1
+
+    return sum(students)-2
