0feb54c04774a00c2eb348bd359c3088d75eebc8
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-26 12:32:17 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.31 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ Python3/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,18 +1,29 @@
 def solution(wallpaper):
-    lux,luy, rdx,rdy = -1,-1,-1,-1
-        
-    for idx,i in enumerate(wallpaper): # 시작 지점 찾기.
-        if lux == -1 and '#' in i : 
-            lux = idx
-            luy = i.index('#')
-        elif '#' in i :
-            if i.index('#') < luy : luy = i.index('#')
-        
-        if '#' in i and i.rindex('#') > rdy : rdy = i.rindex('#')
-        
-    for idx,i in enumerate(wallpaper[::-1]):
-        if '#' in i :
-            rdx = len(wallpaper)-idx
-            break
+    x = []
+    y = []
     
-    return [lux, luy, rdx, rdy+1]
+    for idxY,i in enumerate(wallpaper):
+        for idxX,j in enumerate(i):
+            if j=='#':
+                x.append(idxX)
+                y.append(idxY)
+    
+    return [min(y), min(x), max(y)+1, max(x)+1]
+
+#     lux,luy, rdx,rdy = -1,-1,-1,-1
+        
+#     for idx,i in enumerate(wallpaper): # 시작 지점 찾기.
+#         if lux == -1 and '#' in i : 
+#             lux = idx
+#             luy = i.index('#')
+#         elif '#' in i :
+#             if i.index('#') < luy : luy = i.index('#')
+        
+#         if '#' in i and i.rindex('#') > rdy : rdy = i.rindex('#')
+        
+#     for idx,i in enumerate(wallpaper[::-1]):
+#         if '#' in i :
+#             rdx = len(wallpaper)-idx
+#             break
+    
+#     return [lux, luy, rdx, rdy+1]
