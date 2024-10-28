661ea03a172a613fc006acaf2f8a8b231f53ee26
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-28 14:46:11 +0900

    [level 2] Title: 숫자의 표현, Time: 4.84 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ Python3/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,3 +1,11 @@
 def solution(num):
-    
-    return len([i  for i in range(1,num+1,2) if num % i == 0])
+    cnt = 0
+    for i in range(1,num//2+1):
+        total = 0
+        for j in range(i,num):
+            total += j
+            if total == num : cnt += 1; break
+            if total > num : break
+            
+    return cnt + 1
+    # return len([i for i in range(1,num+1,2) if num % i == 0])
