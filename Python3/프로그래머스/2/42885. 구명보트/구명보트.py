6b944111e2bdbc70fe817ecfb2a0a8b0aa63277e
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-10-28 17:19:34 +0900

    [level 2] Title: 구명보트, Time: 8.99 ms, Memory: 10.8 MB -BaekjoonHub

--- Python3/프로그래머스/2/42885. 구명보트/구명보트.py
+++ Python3/프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,12 +1,24 @@
 def solution(people, limit):
     cnt = 0
-    people.sort(reverse=True)
-    first, last = 0, len(people)-1
+    people = sorted(people,reverse=True)
     
-    while first <= last:
-        if people[first] + people[last] > limit: first += 1
-        else : first +=1; last -=1
-        
+    for i in people:
+        total = i
+        if total + people[-1] <= limit : people.pop()
         cnt += 1
-        
+            
     return cnt
+    
+    
+    
+#     cnt = 0
+#     people.sort(reverse=True)
+#     first, last = 0, len(people)-1
+    
+#     while first <= last:
+#         if people[first] + people[last] > limit: first += 1
+#         else : first +=1; last -=1
+        
+#         cnt += 1
+        
+#     return cnt
