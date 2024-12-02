85c66aa7504009e26519ca0b3dfe4d57489c8ff6
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-02 14:50:36 +0900

    Update H－Index.py

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,9 @@
 def solution(citations):
     result = 0
     citations.sort(reverse=True)
-#     for i in citations:
-#         cnt = sum(1 for j in citations if j >= i)
-#         result = max(result, min(i, cnt))
-        
-#     return result
-    answer = max(map(min, enumerate(citations, start=1)))
-    return answer
+    for idx, i in enumerate(citations, start = 1):
+        # cnt = sum(1 for j in citations if j >= i)
+        result = max(result, min(i, idx))
+    return result 
+    # answer = max(map(min, enumerate(citations, start=1)))
+    # return answer
