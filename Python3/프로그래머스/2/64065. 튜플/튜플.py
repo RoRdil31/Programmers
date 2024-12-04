de9adcdd0c5d213e97ba087446306ef2eb9eb187
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-04 15:52:25 +0900

    [level 2] Title: 튜플, Time: 38.03 ms, Memory: 20.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/64065. 튜플/튜플.py
+++ Python3/프로그래머스/2/64065. 튜플/튜플.py
@@ -2,19 +2,5 @@ import re
 from collections import Counter
 def solution(s):
     s = Counter(re.findall('\d+', s))
-    # for v, cnt in s.most_common() : answer.append(int(v))
-    
     return [int(v) for v, cnt in s.most_common()]
     
-#     answer, lst = [], []
-#     for i in s[2:-2].split('},{'): lst.append(i.split(','))
-#     sorted_s = sorted(lst, key = lambda x : len(x))
-
-#     answer.append(sorted_s[0][0])
-#     for sub in sorted_s[1:] :
-#         print(set(sub) - set(answer))
-#         rest = (set(sub) - set(answer))
-#         r = (list(rest)[0])
-#         answer.append(r)
-        
-#     return [int(i) for i in answer]
