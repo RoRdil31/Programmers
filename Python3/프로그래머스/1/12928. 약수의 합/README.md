2e76e8246d15d54badaeb2c53a266b36a922472b
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-04 19:08:08 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/12928. 약수의 합/README.md
+++ Python3/프로그래머스/1/12928. 약수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.17 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 14:40:09
+2024년 12월 04일 19:08:06
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,17 +1,22 @@
-# Basic for문
 def solution(n):
-    sum=0
-    for i in range(1,n+1):
-        sum += i if n%i == 0 else 0
-    return sum
+    a = [i for i in range(1, int(n**0.5)+1) if n//i == n/i]
+    a.extend([n//i for i in a])
+    return sum(set(a))
 
-# list comprehension
-def solution(n):
-    return sum([i for i in range(1,n+1) if n%i ==0])
 
-# sqrt로 for문 연산 줄이는 방법
-import math
-def solution(n):
-    l1 = [i for i in range(1,int(math.sqrt(n))+1) if n%i == 0]
-    l1.extend([n//i for i in l1])
-    return sum(set(l1))
+
+
+
+
+
+
+
+
+
+
+
+# import math
+# def solution(n):
+#     l1 = [i for i in range(1,int(math.sqrt(n))+1) if n%i == 0]
+#     l1.extend([n//i for i in l1])
+#     return sum(set(l1))
