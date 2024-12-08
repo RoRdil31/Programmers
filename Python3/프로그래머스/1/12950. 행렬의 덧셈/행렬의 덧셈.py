773cbe37dedde7a3ba3847ae61bbc04f7d3c8abc
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-08 22:45:00 +0900

    [level 1] Title: 행렬의 덧셈, Time: 20.46 ms, Memory: 22.8 MB -BaekjoonHub

--- Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,16 +1,52 @@
-import numpy as np
-def solution(arr1, arr2):
-    answer = np.array(arr1)+np.array(arr2)
-    return answer.tolist()
-
-
 def solution(arr1, arr2):
     answer = []
-    
-    for i,j in zip(arr1, arr2):
-        sum = []
-        for n,m in zip(i,j):
-            sum.append(n+m)
-        answer.append(sum)
-    
+    for a1, a2 in zip(arr1, arr2):
+        lst = []
+        for e1, e2 in zip(a1, a2):
+            lst.append(e1+e2)
+        answer.append(lst)
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+# def solution(arr1, arr2):
+#     answer = np.array(arr1)+np.array(arr2)
+#     return answer.tolist()
+
+# def solution(arr1, arr2):
+#     answer = []
+    
+#     for i,j in zip(arr1, arr2):
+#         sum = []
+#         for n,m in zip(i,j):
+#             sum.append(n+m)
+#         answer.append(sum)
+    
+#     return answer
