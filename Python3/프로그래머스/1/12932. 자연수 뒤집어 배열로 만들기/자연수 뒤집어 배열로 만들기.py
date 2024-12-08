7737dd5fa46ff1205a4a193ff0bb2d32fa3c9fd6
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-08 21:11:02 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ Python3/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,5 +1,23 @@
-# def solution(n):
-#     return list(reversed([int(i) for i in list(str(n))]))
-
 def solution(n):
-    return [int(i) for i in reversed(str(n))]
+    return [int(i) for i in str(n)[::-1]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # def solution(n):
+# #     return list(reversed([int(i) for i in list(str(n))]))
+
+# def solution(n):
+#     return [int(i) for i in reversed(str(n))]
