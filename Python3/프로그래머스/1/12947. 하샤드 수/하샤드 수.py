345cf7334ad1483e528f9256bbe5b87f8700f1d0
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-08 21:27:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ Python3/프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,2 +1,27 @@
 def solution(x):
-    return x%(sum(int(i) for i in str(x)))==0
+    return True if ( x % sum([int(i) for i in str(x)]) ) == 0 else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(x):
+#     return x%(sum(int(i) for i in str(x)))==0
