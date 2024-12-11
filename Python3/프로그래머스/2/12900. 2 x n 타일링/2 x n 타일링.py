b145f1db93520de72ca12e1a103a13bd0f78f32e
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-11 12:20:24 +0900

    [level 2] Title: 2 x n 타일링, Time: 28.59 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+from collections import deque
+def solution(n):
+    if n==1 : return 1
+    elif n==2 : return 2  
+    dp = deque([1,2])
+    
+    for _ in range(3, n+1):
+        dp.append(dp.popleft() + dp[0])
+    
+    return dp[-1] % 1000000007
