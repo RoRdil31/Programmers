baff666b2858723351426b85e679a30e6bef9f98
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-13 15:49:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.58 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from itertools import chain
+def solution(n, wires):
+    
+    def calc_total(total, i, visited): 
+        for j in dic[i]:
+            if j not in visited:
+                visited.append(j)
+                total = calc_total(total+1, j, visited)
+
+        return total
+    
+    
+    
+    counter = Counter(list(chain(*wires)))
+    counter = counter.most_common()
+    root_len = counter[0][1]
+    roots = [i for i,cnt in counter if root_len == cnt]
+    dic = {i:[] for i in range(1,n+1)}
+    for v1, v2 in wires: dic[v1].append(v2); dic[v2].append(v1)
+    
+    # print(counter)
+    # print(roots)
+    answer = 9999
+    # for root in roots:
+    for root in range(1,n+1):
+        for i in dic[root]:
+            visited = [root, i]
+            g1 = calc_total(1, i, visited)
+            if abs(n - 2*g1) < answer : answer = abs(n - 2*g1)
+    
+    return answer
+
