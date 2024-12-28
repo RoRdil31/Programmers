a1815f234c4b5f6116eeff804f4b87f558cffc91
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 16:51:49 +0900

    [level 1] Title: 모의고사, Time: 3.33 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/42840. 모의고사/모의고사.py
+++ Python3/프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,13 +1,41 @@
 def solution(answers):
-    persons = [[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    scores = []
-    for p in persons : 
-        cnt = 0
-        for idx, i in enumerate(answers):
-            if p[idx%len(p)]==i : cnt += 1
-        scores.append(cnt)
+    s1 = sum(1 for idx,i in enumerate(answers) if p1[idx%len(p1)]==i)
+    s2 = sum(1 for idx,i in enumerate(answers) if p2[idx%len(p2)]==i)
+    s3 = sum(1 for idx,i in enumerate(answers) if p3[idx%len(p3)]==i)
+    max_num = max(s1, s2, s3)
     
-    return [idx+1 for idx,s in enumerate(scores) if s==max(scores)]
+    return [idx+1 for idx,i in enumerate([s1,s2,s3]) if i==max_num]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(answers):
+#     persons = [[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    
+#     scores = []
+#     for p in persons : 
+#         cnt = 0
+#         for idx, i in enumerate(answers):
+#             if p[idx%len(p)]==i : cnt += 1
+#         scores.append(cnt)
+    
+#     return [idx+1 for idx,s in enumerate(scores) if s==max(scores)]
 
 
