371e15e1651661a0d16799dca0a4f44f8d666d20
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 17:53:25 +0900

    [level 1] Title: 소수 찾기, Time: 447.96 ms, Memory: 115 MB -BaekjoonHub

--- Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,7 +1,29 @@
 def solution(n):
-    prime_nums = set(range(2,n+1))
-    
-    for i in range(2, int(n**0.5)+1):
-        if i in prime_nums:
-            prime_nums -= set(range(i*2,n+1,i))
+    prime_nums = set(range(2, n+1))
+    for i in range(2,int(n**0.5)+1): prime_nums -= set(range(i*2, n+1, i))
     return len(prime_nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n):
+#     prime_nums = set(range(2,n+1))
+    
+#     for i in range(2, int(n**0.5)+1):
+#         if i in prime_nums:
+#             prime_nums -= set(range(i*2,n+1,i))
+#     return len(prime_nums)
