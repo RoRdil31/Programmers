175ebcfe15ceabe3289c7f2ce0e5e94cfc058e22
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 15:16:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ Python3/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,2 +1,22 @@
 def solution(n, arr1, arr2):
-    return [str(bin(i | j))[2:].rjust(n,' ').replace('1','#').replace('0',' ') for i,j in zip(arr1,arr2)]
+    return [bin(a1 | a2)[2:].rjust(n).replace('1','#').replace('0',' ') for a1, a2 in zip(arr1, arr2)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n, arr1, arr2):
+#     return [str(bin(i | j))[2:].rjust(n,' ').replace('1','#').replace('0',' ') for i,j in zip(arr1,arr2)]
