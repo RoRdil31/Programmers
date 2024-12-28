86adda36f3f8a36f01023981ca2a38351a61580a
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 13:56:15 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/131705. 삼총사/삼총사.py
+++ Python3/프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,4 +1,27 @@
 from itertools import combinations as comb
 def solution(number):
+    cnt = 0
+    for c in comb(number,3):
+        if sum(c) == 0: cnt += 1
     
-    return sum(not sum(c) for c in comb(number,3))
+    return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from itertools import combinations as comb
+# def solution(number):
+#     return sum(not sum(c) for c in comb(number,3))
