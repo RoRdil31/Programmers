ad86cdd832f14d9decc93da8c65c136600f00823
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 14:29:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ Python3/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,7 +1,29 @@
 def solution(food):
-    answer = ''
-    for idx,n in enumerate(food):
-        answer += str(idx)*(n//2)
+    ans = ''
+    for idx, i in enumerate(food[1:], start=1):
+        cnt = i//2
+        ans += str(idx)*cnt
     
-    # return answer+'0'+''.join(sorted(answer,reverse=True))
-    return answer+'0'+answer[::-1]
+    return ans + '0' + ans[::-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(food):
+#     answer = ''
+#     for idx,n in enumerate(food):
+#         answer += str(idx)*(n//2)
+    
+#     # return answer+'0'+''.join(sorted(answer,reverse=True))
+#     return answer+'0'+answer[::-1]
