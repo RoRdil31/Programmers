31cd7da2db2133d6fffc5c176e0b23d3ad12619e
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 13:44:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ Python3/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,9 +1,41 @@
 def solution(s):
-    result = []
+    answer = []
+    s = s.lower()
     for word in s.split(' '):
-        new_word = ''
+        ans = ''
         for idx,w in enumerate(word):
-            if idx%2 ==0 : new_word += w.upper()
-            else : new_word += w.lower()
-        result.append(new_word)
-    return ' '.join(result)
+            if idx%2 == 0: ans += w.upper()
+            else: ans += w
+        answer.append(ans)
+    return ' '.join(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(s):
+#     result = []
+#     for word in s.split(' '):
+#         new_word = ''
+#         for idx,w in enumerate(word):
+#             if idx%2 ==0 : new_word += w.upper()
+#             else : new_word += w.lower()
+#         result.append(new_word)
+#     return ' '.join(result)
