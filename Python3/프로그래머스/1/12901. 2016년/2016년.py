7ad2c2737b9348057132a32a6392d7af7b290a8c
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 15:26:03 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/12901. 2016년/2016년.py
+++ Python3/프로그래머스/1/12901. 2016년/2016년.py
@@ -1,3 +1,26 @@
-import datetime
-def solution(a, b):
-    return datetime.date(2016,a,b).strftime('%a').upper()
+import datetime as dt
+def solution(a,b):
+    return dt.date(2016,a,b).strftime('%a').upper()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import datetime
+# def solution(a, b):
+#     return datetime.date(2016,a,b).strftime('%a').upper()
