d6d8cf672478cf7a23e7b0b23f0b022f7c6def59
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 14:18:19 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.34 ms, Memory: 10.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ Python3/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,11 +1,38 @@
 def solution(s):
-    answer = []
+    ans = []
     record = {}
-    for idx, c in enumerate(s):
-        if c in record:
-            answer.append(idx - record[c])
-        else :
-            answer.append(-1)
-        record[c] = idx
+    for idx,w in enumerate(s):
+        if w in record.keys():
+            ans.append(idx - record[w])
+        else : ans.append(-1)
+        record[w] = idx
+        
+    return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(s):
+#     answer = []
+#     record = {}
+#     for idx, c in enumerate(s):
+#         if c in record:
+#             answer.append(idx - record[c])
+#         else :
+#             answer.append(-1)
+#         record[c] = idx
             
-    return answer
+#     return answer
