514caec3f470c94d10da99f635b2f6e1c008dc43
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 19:00:01 +0900

    [level 1] Title: 실패율, Time: 13.67 ms, Memory: 18.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/42889. 실패율/실패율.py
+++ Python3/프로그래머스/1/42889. 실패율/실패율.py
@@ -1,12 +1,44 @@
+from collections import Counter
 def solution(N, stages):
-    odd = {}
+    ans = {i:0 for i in range(1,N+1)}
+    counter = Counter(stages)
     l = len(stages)
-    for i in range(1,N+1):
-        if l != 0:
-            cnt_num = stages.count(i)
-            odd[i] = cnt_num / l
-            l -= cnt_num
-        else:
-            odd[i] = 0
-    answer = sorted(odd, key = lambda x:-odd[x])
-    return answer #[i[0] for i in answer]
+    
+    for i in sorted(counter):
+        if i==N+1 : break
+        ans[i] = counter[i] / l
+        l -= counter[i]
+    
+    return sorted(ans, key = lambda x : ans[x], reverse=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(N, stages):
+#     odd = {}
+#     l = len(stages)
+    
+#     for i in range(1,N+1):
+#         if l != 0:
+#             odd[i] = stages.count(i) / l
+#             l -= stages.count(i)
+#         else: odd[i] = 0
+        
+#     return sorted(odd, key = lambda x:-odd[x])
