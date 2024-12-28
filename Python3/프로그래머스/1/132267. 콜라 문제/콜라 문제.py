6135a549d91de8a827f9b29535256b87cde36eda
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 15:03:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.45 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ Python3/프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,32 @@
-# def solution(a, b, n):
-#     answer = 0
-#     while n//a > 0 :
-#         answer += (n//a)*b
-#         n = (n//a)*b + n%a
+def solution(a, b, n):
+    ans = 0
+    while n >= a:
+        ans += (n//a)*b
+        n = (n//a)*b + n%a
+
+    return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # def solution(a, b, n):
+# #     answer = 0
+# #     while n//a > 0 :
+# #         answer += (n//a)*b
+# #         n = (n//a)*b + n%a
         
-#     return answer
-solution = lambda a, b, n : max(n-b, 0) // (a-b)*b
+# #     return answer
+# solution = lambda a, b, n : max(n-b, 0) // (a-b)*b
