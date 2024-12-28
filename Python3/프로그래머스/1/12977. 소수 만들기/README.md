3489c5761239cbcb86173c6f25bfc392544aaab8
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 18:04:59 +0900

    [level 1] Title: 소수 만들기, Time: 106.92 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12977. 소수 만들기/README.md
+++ Python3/프로그래머스/1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 45.12 ms
+메모리: 10.3 MB, 시간: 106.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 12:06:23
+2024년 12월 28일 18:04:56
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ Python3/프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,11 +1,37 @@
 from itertools import combinations as comb
 def solution(nums):
-    answer = 0
-    
-    sums = [sum(c) for c in comb(nums,3)]
-    
-    for n in sums:
-        if not sum(1 for i in range(2,int(n**0.5)+1) if n%i==0):
-            answer += 1
+    cnt = 0
+    for i in comb(nums,3):
+        if not sum(1 for j in range(2, int(sum(i)**0.5)+1) if sum(i)%j==0): cnt += 1
+        
+    return cnt
 
-    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from itertools import combinations as comb
+# def solution(nums):
+#     answer = 0
+    
+#     sums = [sum(c) for c in comb(nums,3)]
+    
+#     for n in sums:
+#         if not sum(1 for i in range(2,int(n**0.5)+1) if n%i==0):
+#             answer += 1
+
+#     return answer
