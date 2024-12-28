0f0e262e60c186d51dfb5442cf13222fcea37847
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 16:17:25 +0900

    [level 1] Title: 기사단원의 무기, Time: 1215.83 ms, Memory: 10.9 MB -BaekjoonHub

--- Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,8 +1,35 @@
-def solution(num, limit, p):
-    answer = 0
-    for n in range(1,num+1):
-        length =  sum(1 for i in range(1,int(n**0.5)+1) if n%i==0) * 2
-        if n**0.5 == int(n**0.5) : length -= 1
-        answer += p if length > limit else length
+def solution(number, limit, power):
+    nums = []
+    for i in range(1,number+1):
+        cnt = sum(1 for j in range(1,int(i**0.5)+1) if i%j==0)*2
+        if int(i**0.5) == (i**0.5) : cnt -= 1
+        if cnt > limit: cnt = power
+        nums.append(cnt)
+    
+    return sum(nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(num, limit, p):
+#     answer = 0
+#     for n in range(1,num+1):
+#         length =  sum(1 for i in range(1,int(n**0.5)+1) if n%i==0) * 2
+#         if n**0.5 == int(n**0.5) : length -= 1
+#         answer += p if length > limit else length
             
-    return answer
+#     return answer
