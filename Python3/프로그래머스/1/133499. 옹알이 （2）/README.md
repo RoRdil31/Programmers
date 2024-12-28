01c147e52727e5d30497984dc7d7bdf298c756f4
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 18:29:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.56 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/133499. 옹알이 （2）/README.md
+++ Python3/프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.13 ms
+메모리: 10.1 MB, 시간: 0.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 11:42:26
+2024년 12월 28일 18:29:35
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ Python3/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,18 +1,61 @@
+from collections import deque
 def solution(babbling):
-    can_s = ['aya','ye','woo','ma']
-    cant_s = ['ayaaya','yeye','woowoo','mama']
+    can = ["aya", "ye", "woo", "ma"]
+    cant = ["ayaaya", "yeye", "woowoo", "mama"]
     cnt = 0
-    
     for word in babbling:
-        
-        for i in cant_s : 
-            if i in word : word = word.replace(i,'x')
-        
-        for i in can_s : 
-            if i in word : word = word.replace(i,' ')
-        
-        word = word.replace(' ','')
-        
-        if word == '' : cnt += 1
-        
+        no_skip = True
+        for x in cant: 
+            if x in word : no_skip=False; break
+            
+        if no_skip:
+            queue = deque()
+            word = deque(word)
+            while word:
+                queue.append(word.popleft())
+                if ''.join(queue) in can : queue.clear()
+            if not queue : cnt += 1
+    
     return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(babbling):
+#     can_s = ['aya','ye','woo','ma']
+#     cant_s = ['ayaaya','yeye','woowoo','mama']
+#     cnt = 0
+    
+#     for word in babbling:
+        
+#         for i in cant_s : 
+#             if i in word : word = word.replace(i,'x')
+        
+#         for i in can_s : 
+#             if i in word : word = word.replace(i,' ')
+        
+#         word = word.replace(' ','')
+        
+#         if word == '' : cnt += 1
+        
+#     return cnt
