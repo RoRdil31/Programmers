a6ac7b7f49128ba1e140db748c9aa48053851226
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-28 13:31:38 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ Python3/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,38 @@
-def solution(n, m):
+import math
+def solution(n,m):
+    gcd = math.gcd(n,m)
     
-    gcd = 1
-    for i in range(min(n,m),0,-1):
-        if m%i == 0 and n%i == 0: 
-            gcd = i
-            break
+    return [gcd, n*m/gcd]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n, m):
     
-    return [gcd,(n*m)/gcd]
+#     gcd = 1
+#     for i in range(min(n,m),0,-1):
+#         if m%i == 0 and n%i == 0: 
+#             gcd = i
+#             break
+    
+#     return [gcd,(n*m)/gcd]
