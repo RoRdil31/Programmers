3762d56368db381269b6535c60e94fcde441473e
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-30 23:07:44 +0900

    [level 1] Title: 숫자 짝꿍, Time: 342.24 ms, Memory: 27.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,37 @@
 from collections import Counter
 def solution(X, Y):
     answer = ''
-    x = Counter(X)
-    y = Counter(Y)
+    counter = Counter(X) & Counter(Y)
+    sorted_keys = sorted(counter.keys(), reverse=True)
+    for i in sorted_keys: answer += i*counter[i]
+    # answer = ''.join(sorted(counter.elements(), reverse=True))
+    if answer != '' and answer.count('0') == len(answer) : return '0'
     
-    for i in x :
-        if i in y :
-            answer += i*min(x[i],y[i])
-    if answer !='' and answer.count('0') == len(answer) : return '0'
-    return ''.join(sorted(answer,reverse=True)) if answer!='' else "-1"
+    return answer if answer else '-1'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+# def solution(X, Y):
+#     answer = ''
+#     x = Counter(X)
+#     y = Counter(Y)
+    
+#     for i in x :
+#         if i in y :
+#             answer += i*min(x[i],y[i])
+#     if answer !='' and answer.count('0') == len(answer) : return '0'
+#     return ''.join(sorted(answer,reverse=True)) if answer!='' else "-1"
