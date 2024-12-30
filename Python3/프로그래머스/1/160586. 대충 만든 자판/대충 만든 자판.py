06ecf0f805d9a86f2a778e2cfee28fd046a85637
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-30 22:14:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 28.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,17 +1,45 @@
 def solution(keymap, targets):
     answer = []
-    
-    for target in targets:
-        sum_, fail = 0, False
-        for t in target:
-            min_index = 999
-            for key in keymap:
-                min_index = key.find(t) if key.find(t) != -1 and key.find(t) <= min_index else min_index
-            
-            if min_index == 999 : fail = True ; break
-            sum_ += min_index+1
-        
-        if fail : answer.append(-1)
-        else : answer.append(sum_)
+    for tg in targets:
+        time = 0
+        for t in tg:
+            tmp = [km.index(t) for km in keymap if t in km]
+            if tmp : time += min(tmp)+1
+            else : time = -1; break
+        answer.append(time)
     
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(keymap, targets):
+#     answer = []
+    
+#     for target in targets:
+#         sum_, fail = 0, False
+#         for t in target:
+#             min_index = 999
+#             for key in keymap:
+#                 min_index = key.find(t) if key.find(t) != -1 and key.find(t) <= min_index else min_index
+            
+#             if min_index == 999 : fail = True ; break
+#             sum_ += min_index+1
+        
+#         if fail : answer.append(-1)
+#         else : answer.append(sum_)
+    
+#     return answer
