eabe5c91598f3a2f9bec170729f19f9c37a492cd
Minji <117567368+RoRdil31@users.noreply.github.com>
2024-12-30 20:43:31 +0900

    [level 2] Title: 무인도 여행, Time: 6.69 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,32 @@
+from collections import deque
+def solution(maps):
+    maps.insert(0,'X'*len(maps[0]))
+    maps.append('X'*len(maps[0]))
+    maps = ['X'+m+'X' for m in maps]
+    directions = [(-1,0),(1,0),(0,-1),(0,1)] # 상하좌우
+    rows, cols = len(maps), len(maps[0])
+    grid = [list(row) for row in maps]
+    
+    def bfs(x, y):
+        queue = deque([(x, y)])
+        score = int(grid[x][y])
+        grid[x][y] = 'X'
+        
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in directions:
+                nx, ny = x+dx, y+dy
+                if grid[nx][ny]!='X':
+                    queue.append((nx, ny))
+                    score += int(grid[nx][ny])
+                    grid[nx][ny] = 'X'
+        
+        return score
+    
+    answer = []
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] != 'X': answer.append(bfs(i,j))
+    
+    
+    return sorted(answer) if answer else [-1]
