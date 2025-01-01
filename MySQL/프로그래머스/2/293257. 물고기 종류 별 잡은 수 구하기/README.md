73b055569424af86db3d047fbd94c52a1878d84e
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 22:03:12 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/README.md
+++ MySQL/프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/README.md
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 13:26:31
+2025년 01월 01일 22:03:09
 
 ### 문제 설명
 
@@ -65,13 +65,10 @@
 <td>INTEGER</td>
 <td>FALSE</td>
 </tr>
-<tr>
-<td>FISH_NAME</td>
-<td>VARCHAR</td>
-<td>FALSE</td>
-</tr>
 </tbody>
       </table>
+<p>| FISH_NAME | VARCHAR | FALSE |</p>
+
 <hr>
 
 <h5>문제</h5>
--- MySQL/프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ MySQL/프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -1,6 +1,25 @@
 SELECT COUNT(*) AS FISH_COUNT, FISH_NAME
-    FROM FISH_INFO A
-    JOIN FISH_NAME_INFO B
-      ON A.FISH_TYPE = B.FISH_TYPE
-    GROUP BY FISH_NAME
+    FROM FISH_INFO AS F, FISH_NAME_INFO AS N
+    WHERE F.FISH_TYPE = N.FISH_TYPE
+    GROUP BY 2
     ORDER BY 1 DESC
+
+
+
+# F :  ID, 물고기의 종류(숫자), 잡은 물고기의 길이(cm), 물고기를 잡은 날짜
+
+# N : 물고기의 종류(숫자), 물고기의 이름(문자)
+
+
+
+
+
+
+
+
+# SELECT COUNT(*) AS FISH_COUNT, FISH_NAME
+#     FROM FISH_INFO A
+#     JOIN FISH_NAME_INFO B
+#       ON A.FISH_TYPE = B.FISH_TYPE
+#     GROUP BY FISH_NAME
+#     ORDER BY 1 DESC
