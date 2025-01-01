8e97ebd5ede5ff992ed7bdae8e3c6dcac3c5f26b
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 15:02:39 +0900

    [level 1] Title: 햄버거 만들기, Time: 272.17 ms, Memory: 26.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ Python3/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,18 +1,45 @@
 def solution(ingredient):
-    answer = 0
+    cnt = 0
     stack = []
-    
     for i in ingredient:
-        stack.append(i) 
-        if stack[-4:] == [1,2,3,1] : answer += 1; del stack[-4:]
+        stack.append(i)
+        if len(stack)>=4 and stack[-4:]==[1,2,3,1]:
+            stack.pop();stack.pop();stack.pop();stack.pop();
+            cnt += 1
     
-    return answer
+    return cnt
 
 
-# Time Complexity : O(n^2)
 
-# hamberger = "1231"
-# str_ing = ''.join(map(str,ingredient))
-# while hamberger in str_ing :
-#         answer += 1
-#         str_ing = str_ing.replace(hamberger,'',1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(ingredient):
+#     answer = 0
+#     stack = []
+    
+#     for i in ingredient: # O(n)
+#         stack.append(i) # O(1)
+#         if stack[-4:] == [1,2,3,1] : answer += 1; del stack[-4:] # O(1)
+    
+#     return answer
+
+
+# # Time Complexity : O(n^2)
+# # hamberger = "1231"
+# # str_ing = ''.join(map(str,ingredient))
+# # while hamberger in str_ing :
+# #         answer += 1
+# #         str_ing = str_ing.replace(hamberger,'',1)
