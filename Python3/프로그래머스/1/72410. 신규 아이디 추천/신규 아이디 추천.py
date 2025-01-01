34bc50adec690def3ca080ba3ef9e23f503ef3ac
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 17:24:48 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.29 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,22 +1,55 @@
 import re
 def solution(new_id):
-    # 1,2단계 : 소문자, 숫자, '.', '_', '-' 만 추출.
-    new_id = ''.join(re.findall(r'[a-z0-9._-]', new_id.lower()))
     
-    # 3단계 : 마침표 2번 이상 연속 -> 1개로.
-    new_id = re.sub(r'\.+','.', new_id)
-    
-    # 4단계 : 마침표 처음이나 끝에 있으면 제거.
-    new_id = new_id.strip('.')
-    
-    # 5단계 : 빈 문자열이라면, "a" 대입.
-    if not new_id : new_id = "a"
-    
-    # 6단계 : 길이 >= 16, 제거 후 끝에 마침표 있다면 제거.
-    if len(new_id) >= 16 : new_id = new_id[:15].strip('.')
-    
-    # 7단계 : 길이 <= 2, 길이 3 될 때까지 마지막 문자 붙임.
-    if len(new_id) <= 2 : 
-        new_id += new_id[-1]*(3-len(new_id))
+    new_id = new_id.lower() # 1단계
+    new_id = ''.join(re.findall(r'[a-z0-9-_.]', new_id)) # 2단계
+    new_id = ''.join(re.sub(r'[.]+', '.', new_id)) # 3단계
+    new_id = new_id.strip('.') # 4단계
+    if not new_id : new_id = 'a' # 5단계
+    if len(new_id) >= 16 : new_id = new_id[:15]; new_id = new_id.strip('.') # 6단계
+    if len(new_id) <= 2 : new_id += new_id[-1]*(3 - len(new_id))
     
     return new_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import re
+# def solution(new_id):
+#     # 1,2단계 : 소문자, 숫자, '.', '_', '-' 만 추출.
+#     new_id = ''.join(re.findall(r'[a-z0-9._-]', new_id.lower()))
+    
+#     # 3단계 : 마침표 2번 이상 연속 -> 1개로.
+#     new_id = re.sub(r'\.+','.', new_id)
+    
+#     # 4단계 : 마침표 처음이나 끝에 있으면 제거.
+#     new_id = new_id.strip('.')
+    
+#     # 5단계 : 빈 문자열이라면, "a" 대입.
+#     if not new_id : new_id = "a"
+    
+#     # 6단계 : 길이 >= 16, 제거 후 끝에 마침표 있다면 제거.
+#     if len(new_id) >= 16 : new_id = new_id[:15].strip('.')
+    
+#     # 7단계 : 길이 <= 2, 길이 3 될 때까지 마지막 문자 붙임.
+#     if len(new_id) <= 2 : 
+#         new_id += new_id[-1]*(3-len(new_id))
+    
+#     return new_id
