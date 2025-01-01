6431cad03f7cac5451626552eea2051d4e68ea95
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 15:30:20 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.58 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ Python3/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,19 +1,58 @@
-import numpy as np
+from collections import  deque
 def solution(board, moves):
-    answer = 0
-    stack = []
-    board = np.array(board).T
-    board = (board).tolist()
-    board = [[i for i in row if i!=0] for row in board] # 0(인형없음) 제거.
+    cnt = 0
+    board_t, stack = [], []
     
-    for m in moves:
-        if board[m-1] : 
-            stack.append(board[m-1].pop(0)) # stack에 인형 넣고 board[m-1]에서 해당 인형 제거.
+    for i in range(len(board[0])):
+        lst = deque()
+        for b in board:
+            if b[i]!=0 : lst.append(b[i])
+        board_t.append(lst)
         
-        if len(stack)>=2 and stack[-1]==stack[-2] :
-            answer += 2
-            stack.pop()
-            stack.pop()
+    for m in moves:
+        if board_t[m-1] : stack.append(board_t[m-1].popleft())
+        if len(stack)>=2 and stack[-1]==stack[-2]: del stack[-2:]; cnt += 1
+    
+    return cnt * 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+# def solution(board, moves):
+#     answer = 0
+#     stack = []
+#     board = np.array(board).T
+#     board = (board).tolist()
+#     board = [[i for i in row if i!=0] for row in board] # 0(인형없음) 제거.
+    
+#     for m in moves:
+#         if board[m-1] : 
+#             stack.append(board[m-1].pop(0)) # stack에 인형 넣고 board[m-1]에서 해당 인형 제거.
+        
+#         if len(stack)>=2 and stack[-1]==stack[-2] :
+#             answer += 2
+#             stack.pop()
+#             stack.pop()
     
     
-    return answer
+#     return answer
