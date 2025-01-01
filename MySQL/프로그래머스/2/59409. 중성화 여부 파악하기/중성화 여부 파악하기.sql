489c21e168223d07ae76d5c7bf1aee3d085b62aa
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 21:01:28 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,33 +1,54 @@
-# SELECT ANIMAL_ID, NAME, 중성화
-#     FROM (SELECT *,
-#                 CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
-#                         OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-#                      ELSE 'X' END AS 중성화
-#           FROM ANIMAL_INS
-#           )AS A
-#     ORDER
-#        BY ANIMAL_ID
+SELECT ANIMAL_ID, NAME, 
+        CASE WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O' 
+             ELSE 'X'
+        END AS '중성화'
+    FROM ANIMAL_INS 
+    ORDER BY 1
 
 
 
--- LIKE 사용. 더 간단화
-SELECT ANIMAL_ID, NAME,
-        CASE
-            WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
-                OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-            ELSE 'X'
-        END 중성화
-    FROM ANIMAL_INS
-    ORDER BY ANIMAL_ID
+
+
+
+
+
+
+
+
+
+
+
+
+# # SELECT ANIMAL_ID, NAME, 중성화
+# #     FROM (SELECT *,
+# #                 CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
+# #                         OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+# #                      ELSE 'X' END AS 중성화
+# #           FROM ANIMAL_INS
+# #           )AS A
+# #     ORDER
+# #        BY ANIMAL_ID
+
+
+
+# -- LIKE 사용. 더 간단화
+# # SELECT ANIMAL_ID, NAME,
+# #         CASE
+# #             WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
+# #                 OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+# #             ELSE 'X'
+# #         END 중성화
+# #     FROM ANIMAL_INS
+# #     ORDER BY ANIMAL_ID
     
     
     
--- REGEXP 정규식 사용
+# -- REGEXP 정규식 사용
 
-SELECT ANIMAL_ID, NAME,
-        CASE
-            WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
-            ELSE 'X'
-        END 중성화
-    FROM ANIMAL_INS
-    ORDER BY ANIMAL_ID
+# SELECT ANIMAL_ID, NAME,
+#         CASE
+#             WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
+#             ELSE 'O'
+#         END 중성화
+#     FROM ANIMAL_INS
+#     ORDER BY ANIMAL_ID
