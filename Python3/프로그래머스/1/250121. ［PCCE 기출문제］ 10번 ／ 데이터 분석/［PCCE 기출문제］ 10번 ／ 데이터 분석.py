2b8f181565afaff0567c7ba43c4f8c059520929d
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-01 15:48:51 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.21 ms, Memory: 10.6 MB -BaekjoonHub

--- Python3/프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ Python3/프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,9 +1,33 @@
 def solution(data, ext, val_ext, sort_by):
+    d = {'code':0, 'date':1, 'maximum':2, 'remain':3}
     answer = []
-    criteria = {'code':0,'date':1,'maximum':2,'remain':3}
+    for row in data:
+        if row[d[ext]] < val_ext : answer.append(row)
     
-    for d in data:
-        if d[criteria[ext]] < val_ext:
-            answer.append(d)
+    return sorted(answer, key = lambda x : x[d[sort_by]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(data, ext, val_ext, sort_by):
+#     answer = []
+#     criteria = {'code':0,'date':1,'maximum':2,'remain':3}
+    
+#     for d in data:
+#         if d[criteria[ext]] < val_ext:
+#             answer.append(d)
             
-    return sorted(answer, key = lambda x:x[criteria[sort_by]])
+#     return sorted(answer, key = lambda x:x[criteria[sort_by]])
