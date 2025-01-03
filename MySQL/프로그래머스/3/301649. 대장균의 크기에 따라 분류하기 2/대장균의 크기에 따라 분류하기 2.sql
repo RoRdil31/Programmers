45bb2c84e656f8cc0adb67bad3218e1d08ce6980
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 13:47:10 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,12 +1,42 @@
 SELECT ID, 
-        CASE 
-            WHEN S.RANKS/CNTS <= 0.25 THEN 'CRITICAL'
-            WHEN S.RANKS/CNTS <= 0.5 THEN 'HIGH'
-            WHEN S.RANKS/CNTS <= 0.75 THEN 'MEDIUM'
+        CASE
+            WHEN RANKS <= 0.25 THEN 'CRITICAL'
+            WHEN RANKS <= 0.5 THEN 'HIGH'
+            WHEN RANKS <= 0.75 THEN 'MEDIUM'
             ELSE 'LOW'
         END AS COLONY_NAME
-    FROM (SELECT *, 
-            RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS RANKS, 
-            COUNT(*) OVER() AS CNTS
-          FROM ECOLI_DATA) AS S
+    FROM (SELECT ID, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY) AS RANKS
+            FROM ECOLI_DATA) AS SUB 
     ORDER BY 1
+
+# SELECT ID,SIZE_OF_COLONY, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY)
+#             FROM ECOLI_DATA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # SELECT ID, 
+# #         CASE 
+# #             WHEN S.RANKS/CNTS <= 0.25 THEN 'CRITICAL'
+# #             WHEN S.RANKS/CNTS <= 0.5 THEN 'HIGH'
+# #             WHEN S.RANKS/CNTS <= 0.75 THEN 'MEDIUM'
+# #             ELSE 'LOW'
+# #         END AS COLONY_NAME
+# #     FROM (SELECT *, 
+# #             RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS RANKS, 
+# #             COUNT(*) OVER() AS CNTS
+# #           FROM ECOLI_DATA) AS S
+# #     ORDER BY 1
