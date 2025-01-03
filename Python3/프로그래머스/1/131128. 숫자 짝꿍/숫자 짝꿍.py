c3d3fd286f99f9923aab4171181536172ca76ef2
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 16:00:44 +0900

    [level 1] Title: 숫자 짝꿍, Time: 337.62 ms, Memory: 27.8 MB -BaekjoonHub

--- Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,18 +1,12 @@
 from collections import Counter
 def solution(X, Y):
     answer = ''
-    counter = Counter(X) & Counter(Y)
-    sorted_keys = sorted(counter.keys(), reverse=True)
-    for i in sorted_keys: answer += i*counter[i]
-    # answer = ''.join(sorted(counter.elements(), reverse=True))
-    if answer != '' and answer.count('0') == len(answer) : return '0'
+    a = Counter(X) & Counter(Y)
+    if not a : return '-1'
+
+    for v, cnt in sorted(a.items(), key=lambda x:-int(x[0])): answer += v*int(cnt)
     
-    return answer if answer else '-1'
-
-
-
-
-
+    return answer if answer.count('0')!=len(answer) else '0'
 
 
 
@@ -27,11 +21,32 @@ def solution(X, Y):
 # from collections import Counter
 # def solution(X, Y):
 #     answer = ''
-#     x = Counter(X)
-#     y = Counter(Y)
+#     counter = Counter(X) & Counter(Y)
+#     for i in sorted(counter.keys(), reverse=True): answer += i*counter[i]
     
-#     for i in x :
-#         if i in y :
-#             answer += i*min(x[i],y[i])
-#     if answer !='' and answer.count('0') == len(answer) : return '0'
-#     return ''.join(sorted(answer,reverse=True)) if answer!='' else "-1"
+#     if answer != '' and answer.count('0') == len(answer) : return '0'
+#     return answer if answer else '-1'
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # from collections import Counter
+# # def solution(X, Y):
+# #     answer = ''
+# #     x = Counter(X)
+# #     y = Counter(Y)
+    
+# #     for i in x :
+# #         if i in y :
+# #             answer += i*min(x[i],y[i])
+# #     if answer !='' and answer.count('0') == len(answer) : return '0'
+# #     return ''.join(sorted(answer,reverse=True)) if answer!='' else "-1"
