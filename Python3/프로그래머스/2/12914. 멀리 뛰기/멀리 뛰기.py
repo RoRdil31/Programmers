07e3f22a6d20613512cb7dcc6aa37afac287e081
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 17:09:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 142.84 ms, Memory: 9.96 MB -BaekjoonHub

--- Python3/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ Python3/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,13 +1,42 @@
 import math
-def solution(n): 
-    answer = 0
-    twos = n//2
-    start = twos
+def solution(n):
+    cnt = 0
+    two, one = n//2, n%2
+    while two >= 0:
+        cnt += math.comb(two+one, one)
+        two -= 1
+        one += 2
     
-    if n%2==1 : start += 1
+    return cnt % 1234567
+
+# 2 1, 1 2
+# 111
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import math
+# def solution(n): 
+#     answer = 0
+#     twos = n//2
+#     start = twos
     
-    for i in range(start, n+1): 
-        answer += math.comb(i,twos)
-        twos -= 1
+#     if n%2==1 : start += 1
+    
+#     for i in range(start, n+1): 
+#         answer += math.comb(i,twos)
+#         twos -= 1
         
-    return answer % 1234567
+#     return answer % 1234567
