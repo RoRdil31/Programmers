2c755202623d893265a270a422aae445f2f67012
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 13:01:20 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,10 +1,34 @@
-SELECT COUNT(*)    AS FISH_COUNT, 
-       MAX(LENGTH) AS MAX_LENGTH, 
+SELECT COUNT(*) AS FISH_COUNT,
+       MAX(LENGTH) AS MAX_LENGTH,
        FISH_TYPE
     FROM FISH_INFO
-    GROUP BY FISH_TYPE
-      HAVING AVG(CASE
-                    WHEN LENGTH <= 10 THEN 10
-                    ELSE LENGTH
-                 END) >= 33
-    ORDER BY FISH_TYPE
+    GROUP BY 3
+      HAVING (AVG(CASE
+                    WHEN LENGTH > 10 THEN LENGTH
+                    ELSE 10
+                  END) >= 33)
+    ORDER BY 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT COUNT(*)    AS FISH_COUNT, 
+#        MAX(LENGTH) AS MAX_LENGTH, 
+#        FISH_TYPE
+#     FROM FISH_INFO
+#     GROUP BY FISH_TYPE
+#       HAVING AVG(CASE
+#                     WHEN LENGTH > 10 THEN LENGTH
+#                     ELSE 10
+#                  END) >= 33
+#     ORDER BY FISH_TYPE
