93221b6eed09a9fa3291e0bb24fd432dba7e13c6
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 12:00:21 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,9 +1,31 @@
+SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_OUTS
+    WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
+                                FROM ANIMAL_INS)
+    ORDER BY 1
+
+
+# I : 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+
+# O : 이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부
+
+
+
+
+
+
+
+
+
+
+
+
 # SELECT ANIMAL_ID, NAME
 #     FROM ANIMAL_OUTS 
 #     WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
 #                               FROM ANIMAL_INS)
 #     ORDER BY ANIMAL_ID
     
-SELECT ANIMAL_ID, NAME FROM ANIMAL_OUTS
-EXCEPT
-SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+# SELECT ANIMAL_ID, NAME FROM ANIMAL_OUTS
+# EXCEPT
+# SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
