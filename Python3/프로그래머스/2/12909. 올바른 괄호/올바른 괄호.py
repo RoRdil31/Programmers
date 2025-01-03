649ab578627c1b7b498094929464c61278493199
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 16:09:14 +0900

    [level 2] Title: 올바른 괄호, Time: 23.50 ms, Memory: 10.8 MB -BaekjoonHub

--- Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,42 +1,72 @@
-
+from collections import deque
 def solution(s):
+    s = deque(s)
     stack = []
-    for i in s:
-        if not stack : stack.append(i); continue
-        if stack[-1] =='(' and i==')' : stack.pop()
-        else : stack.append(i)
-    
-    return False if stack else True
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#     HavetoBreak = False
-#     ex = [i for i in ex]
-#     # ex.append('.')
-    
-#     limit = 0
-#     # i = 0
-#     if ex[-1] == '(' or ex[0] == ')': return False
-
-#     cnt1 = 0
-    
-#     for p in ex:
-#         if p == '(': cnt1 += 1
-#         else : cnt1 -= 1
-        
-#         if cnt1 < 0 :
-#             HavetoBreak = True
-#             break
+    while s:
+        stack.append(s.popleft())
+        if len(stack)>=2 and stack[-2]=='(' and stack[-1]==')':
+            del stack[-2:]
             
-#     if HavetoBreak == True or cnt1!=0: return False
-#     else : return True
+    return False if stack else True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(s):
+#     stack = []
+#     for i in s:
+#         if not stack : stack.append(i); continue
+#         if stack[-1] =='(' and i==')' : stack.pop()
+#         else : stack.append(i)
+    
+#     return False if stack else True
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# #     HavetoBreak = False
+# #     ex = [i for i in ex]
+# #     # ex.append('.')
+    
+# #     limit = 0
+# #     # i = 0
+# #     if ex[-1] == '(' or ex[0] == ')': return False
+
+# #     cnt1 = 0
+    
+# #     for p in ex:
+# #         if p == '(': cnt1 += 1
+# #         else : cnt1 -= 1
+        
+# #         if cnt1 < 0 :
+# #             HavetoBreak = True
+# #             break
+            
+# #     if HavetoBreak == True or cnt1!=0: return False
+# #     else : return True
