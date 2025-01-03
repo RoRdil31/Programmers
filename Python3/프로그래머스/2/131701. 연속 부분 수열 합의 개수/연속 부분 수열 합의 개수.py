ae85082c397185115ab53e4d9776ab866920f051
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 16:50:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4364.02 ms, Memory: 72.8 MB -BaekjoonHub

--- Python3/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ Python3/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,11 +1,39 @@
 def solution(elements):
     answer = []
-    circle = elements*2
-    for i,num in enumerate(elements):
-        answer.append(num)
-        for num2 in circle[i+1:i+len(elements)]:
-            num += num2
-            answer.append(num)
-            
+    e = elements*2
+    for i in range(1,len(elements)+1):
+        for j in range(len(elements)):
+            answer.append(sum(e[j:j+i]))
+    
     return len(set(answer))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(elements):
+#     answer = []
+#     circle = elements*2
+#     for i,num in enumerate(elements):
+#         answer.append(num)
+#         for num2 in circle[i+1:i+len(elements)]:
+#             num += num2
+#             answer.append(num)
+            
+#     return len(set(answer))
     
