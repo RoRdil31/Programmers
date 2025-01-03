eef5db88e6bd868705f9d80ad35b99ca134f9879
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 16:26:50 +0900

    [level 2] Title: 귤 고르기, Time: 45.77 ms, Memory: 26.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,39 @@
 from collections import Counter
 def solution(k, tangerine):
     cnt = 0
-    a = Counter(tangerine)
+    counter = Counter(tangerine)
     
-    for item, count in a.most_common():
-        k -= count
+    for _, i in counter.most_common():
+        k -= i
         cnt += 1
-        if k <= 0 : break
+        if k <= 0 : return cnt
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+# def solution(k, tangerine):
+#     cnt = 0
+#     a = Counter(tangerine)
+    
+#     for item, count in a.most_common():
+#         k -= count
+#         cnt += 1
+#         if k <= 0 : break
     
     
-    return cnt
+#     return cnt
