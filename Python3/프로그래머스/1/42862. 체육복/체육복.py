37bd7ee14db19b0415be1561aacdcdd697f8e2ec
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 15:46:25 +0900

    [level 1] Title: 체육복, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/42862. 체육복/체육복.py
+++ Python3/프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,17 @@
 def solution(n, lost, reserve):
-    # students = [1 if i not in lost else 0 for i in range(n+2)]
-    students = [1]*(n+2)
-    for i in lost:
-        if i in reserve: reserve.remove(i); continue
-        students[i] = 0
+    students = [1 if i not in (set(lost) - set(reserve)) else 0 for i in range(n+2)]
+    for i in sorted(set(reserve) - set(lost)):
         
-    for i in sorted(reserve):
-        if students[i-1] == 0 : students[i-1] = 1
-        elif students[i+1] == 0 : students[i+1] = 1
-        
-    return sum(students)-2
+        if students[i-1]==0: students[i-1] = 1
+        elif students[i+1]==0: students[i+1] = 1
+    
+    return sum(students[1:-1])
+
+
+
+
+
+
 
 
 
@@ -26,16 +28,43 @@ def solution(n, lost, reserve):
 
 
 # def solution(n, lost, reserve):
-    
+#     # students = [1 if i not in lost else 0 for i in range(n+2)]
 #     students = [1]*(n+2)
-#     for i in lost : 
-#         if i in reserve : reserve.remove(i);continue
+#     for i in lost:
+#         if i in reserve: reserve.remove(i); continue
 #         students[i] = 0
         
-#     # for i in reserve : 
-#     #     if students[i] == 0 : students[i] = 1 ;reserve.remove(i)
-#     for i in sorted(reserve) :
-#         if  students[i-1] == 0 : students[i-1] = 1
+#     for i in sorted(reserve):
+#         if students[i-1] == 0 : students[i-1] = 1
 #         elif students[i+1] == 0 : students[i+1] = 1
-
+        
 #     return sum(students)-2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # def solution(n, lost, reserve):
+    
+# #     students = [1]*(n+2)
+# #     for i in lost : 
+# #         if i in reserve : reserve.remove(i);continue
+# #         students[i] = 0
+        
+# #     # for i in reserve : 
+# #     #     if students[i] == 0 : students[i] = 1 ;reserve.remove(i)
+# #     for i in sorted(reserve) :
+# #         if  students[i-1] == 0 : students[i-1] = 1
+# #         elif students[i+1] == 0 : students[i+1] = 1
+
+# #     return sum(students)-2
