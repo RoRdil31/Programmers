afe5cd11e666d25130bec37ac2657b4e095c7aa8
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-03 12:13:05 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ MySQL/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,9 +1,35 @@
-SELECT USER_ID, NICKNAME, 
-        CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS 전체주소,
-        CONCAT(SUBSTR(TLNO,1,3),'-',SUBSTR(TLNO,4,4),'-',SUBSTR(TLNO,8,4)) AS 전화번호
-    FROM USED_GOODS_USER
-    WHERE USER_ID IN (SELECT WRITER_ID
-                        FROM USED_GOODS_BOARD
-                        GROUP BY WRITER_ID
-                        HAVING COUNT(BOARD_ID) >= 3)
-    ORDER BY USER_ID DESC
+SELECT USER_ID, NICKNAME,
+       CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS '전체주소',
+       CONCAT(SUBSTR(TLNO,1,3),'-',SUBSTR(TLNO,4,4),'-',SUBSTR(TLNO,8)) AS '전화번호'
+    FROM USED_GOODS_BOARD AS B
+    JOIN USED_GOODS_USER AS U
+      ON B.WRITER_ID = U.USER_ID
+    GROUP BY WRITER_ID HAVING COUNT(BOARD_ID) >= 3
+    ORDER BY 1 DESC
+
+
+
+# B : 게시글 ID, 작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일, 거래상태, 조회수
+
+# U : 회원 ID, 닉네임, 시, 도로명 주소, 상세 주소, 전화번호
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT USER_ID, NICKNAME, 
+#         CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS 전체주소,
+#         CONCAT(SUBSTR(TLNO,1,3),'-',SUBSTR(TLNO,4,4),'-',SUBSTR(TLNO,8,4)) AS 전화번호
+#     FROM USED_GOODS_USER
+#     WHERE USER_ID IN (SELECT WRITER_ID
+#                         FROM USED_GOODS_BOARD
+#                         GROUP BY WRITER_ID
+#                         HAVING COUNT(BOARD_ID) >= 3)
+#     ORDER BY USER_ID DESC
