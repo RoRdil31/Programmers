607ba18d3e56d621f77daa5e7343b7f7beb0d9bf
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-04 14:10:42 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.98 MB -BaekjoonHub

--- Python3/프로그래머스/2/42578. 의상/의상.py
+++ Python3/프로그래머스/2/42578. 의상/의상.py
@@ -1,9 +1,33 @@
 def solution(clothes):
-    result = 1
-    c = {i[1]:1 for i in clothes}
-    for name, s in clothes:
-        c[s] += 1
+    answer = 1
+    d = {i[1]:[None] for i in clothes}
+    for c, i in clothes: d[i].append(c)
+    for k, v in d.items():
+        answer *= len(v)
     
-    for i in c.values():
-        result = result * i
-    return result - 1
+    return answer - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(clothes):
+#     result = 1
+#     c = {i[1]:1 for i in clothes}
+#     for name, s in clothes:
+#         c[s] += 1
+    
+#     for i in c.values():
+#         result = result * i
+#     return result - 1
