0b6dcc81c84385ef7523406df8a9f9e1cb0f9b22
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-04 14:43:12 +0900

    [level 2] Title: H-Index, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,7 +1,36 @@
 def solution(citations):
-    result = 0
-    citations.sort(reverse=True)
-    for idx, i in enumerate(citations, start = 1):
-        # cnt = sum(1 for j in citations if j >= i)
-        result = max(result, min(i, idx))
-    return result 
+    h = -1
+    citations = sorted(citations, reverse=True)
+    for i, c in enumerate(citations, start=1):
+        if i >= c : h = max(i,c); break
+    
+    if h==-1 : return len(citations)
+    return h + citations.count(h) - 1
+            
+    
+
+# 6 5 3 / 1 0 => 3
+# 6 5 3 3 / 0 => 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(citations):
+#     result = 0
+#     citations.sort(reverse=True)
+#     for idx, i in enumerate(citations, start = 1):
+#         # cnt = sum(1 for j in citations if j >= i)
+#         result = max(result, min(i, idx))
+#     return result 
--- Python3/프로그래머스/2/42747. H－Index/README.md
+++ Python3/프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.64 ms
+메모리: 10.2 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 14:56:13
+2025년 01월 04일 14:43:10
 
 ### 문제 설명
 
