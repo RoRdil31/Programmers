01e17b90b571791b2cd9b45142fe903d89f411f5
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-04 15:05:16 +0900

    [level 2] Title: 피로도, Time: 47.74 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/87946. 피로도/피로도.py
+++ Python3/프로그래머스/2/87946. 피로도/피로도.py
@@ -1,10 +1,37 @@
-from itertools import permutations
+from itertools import permutations as perm
 def solution(k, dungeons):
-    max_cnt = 0
-    for perm in permutations(dungeons):
-        cnt, k2 = 0, k
-        for i,j in perm:
-            if k2 >= i : k2 -= j; cnt += 1
-        if cnt > max_cnt : max_cnt = cnt
+    answer = 0
+    for d in perm(dungeons):
+        cnt, energe = 0, k
+        for need, use in d:
+            if energe <= 0 : break
+            if need <= energe : 
+                energe -= use
+                cnt += 1
+        answer = max(answer, cnt)
+            
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from itertools import permutations
+# def solution(k, dungeons):
+#     max_cnt = 0
+#     for perm in permutations(dungeons):
+#         cnt, k2 = 0, k
+#         for i,j in perm:
+#             if k2 >= i : k2 -= j; cnt += 1
+#         if cnt > max_cnt : max_cnt = cnt
         
-    return max_cnt
+#     return max_cnt
