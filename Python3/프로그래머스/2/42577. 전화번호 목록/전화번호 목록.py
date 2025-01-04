c175914aefb8ca99de52ed01df249eb73afa1698
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-04 15:54:36 +0900

    [level 2] Title: 전화번호 목록, Time: 95.62 ms, Memory: 30.7 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,31 @@
 def solution(phone_book):
-    answer = True
-    phone_book.sort() # = sorted(phone_book, key = lambda x : len(x))
-    for i in range(len(phone_book) - 1) :
-        if phone_book[i+1].startswith(phone_book[i]) : return False
-    return answer
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]): return False
+    
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(phone_book):
+#     answer = True
+#     phone_book.sort()
+#     for i in range(len(phone_book) - 1) :
+#         if phone_book[i+1].startswith(phone_book[i]) : return False
+#     return answer
