d15fbd3901d151a589bebf470f90b69b510572a5
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-05 15:34:45 +0900

    [level 2] Title: 더 맵게, Time: 1991.21 ms, Memory: 51.9 MB -BaekjoonHub

--- Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,13 +1,44 @@
 import heapq
-def solution(scov, K):
+def solution(sco, K):
     cnt = 0
-    heapq.heapify(scov)
-    while len(scov) > 1:
-        m1 = heapq.heappop(scov)
-        m2 = heapq.heappop(scov)
-        if (m1 >= K) : return cnt
-        new = m1 + m2*2
-        heapq.heappush(scov, new)
+    heapq.heapify(sco)
+    while len(sco) >= 2 and sco[0] < K:
+        m1 = heapq.heappop(sco)
+        m2 = heapq.heappop(sco)
+        heapq.heappush(sco, m1+m2*2)
         cnt += 1
+    
+    return cnt if sco[0]>=K else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import heapq
+# def solution(scov, K):
+#     cnt = 0
+#     heapq.heapify(scov)
+#     while len(scov) > 1:
+#         m1 = heapq.heappop(scov)
+#         m2 = heapq.heappop(scov)
+#         if (m1 >= K) : return cnt
+#         new = m1 + m2*2
+#         heapq.heappush(scov, new)
+#         cnt += 1
         
-    return cnt if scov[0] >= K else -1
+#     return cnt if scov[0] >= K else -1
