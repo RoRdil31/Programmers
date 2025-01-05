941c3a2d725daf35986255244731e2da3a92fd7a
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-05 12:31:23 +0900

    [level 2] Title: 타겟 넘버, Time: 358.85 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,47 @@
 from itertools import product
 def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
+    cnt = 0
+    nums = [(-x,x) for x in numbers]
+    for i in product(*nums):
+        if sum(i) == target: cnt += 1
+    
+    return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from itertools import product
 # def solution(numbers, target):
-#     global answer
-#     answer = 0
-#     def dfs(i,total):
-#         global answer
-#         if (i==len(numbers)):
-#             if total==target:
-#                 answer+=1
-#             return
-#         dfs(i+1,total+numbers[i])    
-#         dfs(i+1,total-numbers[i])
-#         return
-#     dfs(0,0)
-#     return answer
+#     l = [(x, -x) for x in numbers]
+#     print(l)
+#     s = list(map(sum, product(*l)))
+#     print(s)
+#     return s.count(target)
+# # def solution(numbers, target):
+# #     global answer
+# #     answer = 0
+# #     def dfs(i,total):
+# #         global answer
+# #         if (i==len(numbers)):
+# #             if total==target:
+# #                 answer+=1
+# #             return
+# #         dfs(i+1,total+numbers[i])    
+# #         dfs(i+1,total-numbers[i])
+# #         return
+# #     dfs(0,0)
+# #     return answer
