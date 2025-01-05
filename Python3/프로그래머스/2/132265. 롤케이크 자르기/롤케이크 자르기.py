8a8901ec2b1e4c37f73591df88678ff717db6e33
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-05 14:31:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 615.80 ms, Memory: 50.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ Python3/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,12 +1,38 @@
 from collections import Counter
 def solution(topping):
-    answer = 0
-    p1 = Counter(topping)
-    p2 = set()
+    cnt = 0
+    p2 = Counter(topping)
+    p1 = set()
     for i in topping:
-        p1[i] -= 1
-        if p1[i] == 0 : del p1[i]
-        p2.add(i)
-        if len(p1) == len(p2) : answer += 1
+        p2[i] -= 1
+        if p2[i] == 0 : del p2[i]
+        p1.add(i)
+        if len(p1) == len(p2): cnt += 1
+        
+    return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+# def solution(topping):
+#     answer = 0
+#     p1 = Counter(topping)
+#     p2 = set()
+#     for i in topping:
+#         p1[i] -= 1
+#         if p1[i] == 0 : del p1[i]
+#         p2.add(i)
+#         if len(p1) == len(p2) : answer += 1
     
-    return answer
+#     return answer
