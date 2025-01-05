64875a32b388ff4aadaa71f3e291b18295eb1e96
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-05 15:51:10 +0900

    [level 2] Title: 2 x n 타일링, Time: 26.57 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ Python3/프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,10 +1,35 @@
-from collections import deque
 def solution(n):
-    if n==1 : return 1
-    elif n==2 : return 2  
-    dp = deque([1,2])
+    n1, n2 = 0,1
+    for i in range(n):
+        n2, n1 = n1+n2, n2
     
-    for _ in range(3, n+1):
-        dp.append(dp.popleft() + dp[0])
+    return n2 % 1000000007
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+# def solution(n):
+#     if n==1 : return 1
+#     elif n==2 : return 2  
+#     dp = deque([1,2])
     
-    return dp[-1] % 1000000007
+#     for _ in range(3, n+1):
+#         dp.append(dp.popleft() + dp[0])
+    
+#     return dp[-1] % 1000000007
--- Python3/프로그래머스/2/12900. 2 x n 타일링/README.md
+++ Python3/프로그래머스/2/12900. 2 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 2 x n 타일링 - 12900 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 28.59 ms
+메모리: 10.2 MB, 시간: 26.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 11일 12:20:22
+2025년 01월 05일 15:51:07
 
 ### 문제 설명
 
