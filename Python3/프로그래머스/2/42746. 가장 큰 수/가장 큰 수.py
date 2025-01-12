43d30f464a4524240442c7ac8879f3a9a22cbaf5
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-12 17:43:11 +0900

    [level 2] Title: 가장 큰 수, Time: 132.73 ms, Memory: 26.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ Python3/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,30 @@
 def solution(numbers):
-    numbers = sorted([str(n) for n in numbers], key = lambda x : x*3, reverse=True)
+    answer = ''
+    nums = [(str(i)*4,len(str(i))) for i in numbers]
+    for i in sorted(nums, reverse=True): answer += i[0][:i[1]]
     
-    return (''.join(numbers)).lstrip('0') if (''.join(numbers)).lstrip('0')!='' else '0'  # str(int(''.join(numbers)))
+    return answer if len(answer)!=answer.count('0') else '0'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(numbers):
+#     numbers = sorted([str(n) for n in numbers], key = lambda x : x*3, reverse=True)
+    
+#     return (''.join(numbers)).lstrip('0') if (''.join(numbers)).lstrip('0')!='' else '0'  # str(int(''.join(numbers)))
 
