d6cf2a444064773d6fcaeaefc211a5466ad1ba2a
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-12 18:27:53 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ Python3/프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,9 @@
 def solution(n,a,b):
     cnt = 0
-    while a!=b :
-        a, b = (a+1)//2, (b+1)//2
+    l, r = min(a,b), max(a,b)
+    while l != r:
+        l, r = (l+1)//2, (r+1)//2
         cnt += 1
-    
     return cnt
 
 
