00e336fc65162a32fffb79fb9c5ac09786b9aadf
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-14 20:01:34 +0900

    [level 2] Title: 소수 찾기, Time: 33.76 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,14 @@
 from itertools import permutations as perm
 def solution(numbers):
-    prime, cnt = [], 0
+    answer, nums = 0, set()
     for i in range(1, len(numbers)+1):
-        for j in perm(numbers, i):
-            prime.append(int(''.join(j)))
-            
-    for num in set(prime):
-        if num == 0 or num == 1: continue
-        for i in range(2, int(num**0.5)+1):
-            if num%i ==0: cnt -= 1; break
-        cnt += 1
-        
-    return cnt
+        for j in perm(numbers,i):
+            nums.add(int(''.join(j)))
+    nums -= {0,1}
+    for num in nums:
+        if not sum(1 for i in range(2, int(num**0.5)+1) if num%i == 0): answer += 1
+    
+    return answer
 
 
 
@@ -31,16 +28,45 @@ def solution(numbers):
 
 
 
-# from itertools import permutations
+
+# from itertools import permutations as perm
 # def solution(numbers):
-#     cnt = 0
-#     availables = set()
-        
-#     for i in range(len(numbers)):
-#         availables |= set(map(int, map("".join, permutations(list(numbers), i + 1))))
-        
-#     availables -= set(range(0, 2))
-#     for i in availables:
-#         if not sum(1 for j in range(2,int(i**0.5)+1) if i%j==0) : cnt += 1
+#     prime, cnt = [], 0
+#     for i in range(1, len(numbers)+1):
+#         for j in perm(numbers, i):
+#             prime.append(int(''.join(j)))
+            
+#     for num in set(prime):
+#         if num == 0 or num == 1: continue
+#         # if not sum(1 for j in range(2,int(num**0.5)+1) if num%j==0) : cnt += 1
+#         for i in range(2, int(num**0.5)+1):
+#             if num%i ==0: cnt -= 1; break
+#         cnt += 1
         
 #     return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # from itertools import permutations
+# # def solution(numbers):
+# #     cnt = 0
+# #     availables = set()
+        
+# #     for i in range(len(numbers)):
+# #         availables |= set(map(int, map("".join, permutations(list(numbers), i + 1))))
+        
+# #     availables -= set(range(0, 2))
+# #     for i in availables:
+# #         if not sum(1 for j in range(2,int(i**0.5)+1) if i%j==0) : cnt += 1
+        
+# #     return cnt
