be7c115cde220b00eeb9ce55e71bf62ddf3b79bf
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-16 16:45:06 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 150.96 ms, Memory: 53.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ Python3/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,21 +1,51 @@
 from math import gcd
 from functools import reduce
-def solution(arrayA, arrayB):
+def solution(A, B):
     answer = []
+    def check(a, other):
+        for i in other:
+            if i%a == 0: return -1
+        return a
     
-    def check(gcd_value, array):
-        for num in array:
-            if num % gcd_value == 0 : return False
-        return True
+    answer.append(check(reduce(gcd, A), B))
+    answer.append(check(reduce(gcd, B), A))
     
-    gcd1 = reduce(gcd, arrayA)
-    p1 = check(gcd1, arrayB)
-    gcd2 = reduce(gcd, arrayB)
-    p2 = check(gcd2, arrayA)
+    return max(answer) if max(answer)!=-1 else 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from math import gcd
+# from functools import reduce
+# def solution(arrayA, arrayB):
+#     answer = []
     
-    if p1&p2 : return max(gcd1, gcd2)
-    elif p1 : return gcd1
-    elif p2 : return gcd2
-    else : return 0
+#     def check(gcd_value, array):
+#         for num in array:
+#             if num % gcd_value == 0 : return False
+#         return True
     
-    return 0
+#     gcd1 = reduce(gcd, arrayA)
+#     p1 = check(gcd1, arrayB)
+#     gcd2 = reduce(gcd, arrayB)
+#     p2 = check(gcd2, arrayA)
+    
+#     if p1&p2 : return max(gcd1, gcd2)
+#     elif p1 : return gcd1
+#     elif p2 : return gcd2
+#     else : return 0
+    
+#     return 0
