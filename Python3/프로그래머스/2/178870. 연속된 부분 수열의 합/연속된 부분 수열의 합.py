0427f3e33de45c0840945f948332ddb0dca4b9be
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-16 13:55:09 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 424.69 ms, Memory: 51.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ Python3/프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,21 +1,21 @@
-def solution(sequence, k):
-    total,s1,s2 = 0,-1,len(sequence)-1
-    answers = []
+# def solution(sequence, k):
+#     total,s1,s2 = 0,-1,len(sequence)-1
+#     answers = []
     
-    for i in range(len(sequence)-1, -1, -1):
-        v = sequence[i]
-        if total + v >= k:
-            if total + v == k: answers.append([i,s2])
-            total = total - sequence[s2] + v
-            s2 -= 1
-        else: total += v
+#     for i in range(len(sequence)-1, -1, -1):
+#         v = sequence[i]
+#         if total + v >= k:
+#             if total + v == k: answers.append([i,s2])
+#             total = total - sequence[s2] + v
+#             s2 -= 1
+#         else: total += v
     
-    final = 0
-    min_diff = answers[0][1] - answers[0][0]
-    for answer in answers:
-        if answer[1]-answer[0] == min_diff : final = answer
+#     final = 0
+#     min_diff = answers[0][1] - answers[0][0]
+#     for answer in answers:
+#         if answer[1]-answer[0] == min_diff : final = answer
         
-    return final
+#     return final
 
 
 
@@ -36,17 +36,17 @@ def solution(sequence, k):
 
 
 
-# def solution(seq, k):
-#     total, limit_len, idx = 0, 0, -1
-#     if k in seq : return [seq.index(k), seq.index(k)]
+def solution(seq, k):
+    total, limit_len, idx = 0, 0, -1
+    if k in seq : return [seq.index(k), seq.index(k)]
     
-#     for i in range(len(seq)-1, -1, -1):
-#         total += seq[i]
-#         while total > k :
-#             total -= seq.pop()
+    for i in range(len(seq)-1, -1, -1):
+        total += seq[i]
+        while total > k :
+            total -= seq.pop()
             
-#         if total == k : 
-#             if limit_len and len(seq)-1-i > limit_len : return [idx, idx+limit_len]
-#             else : limit_len = len(seq) - 1 - i; idx = i
+        if total == k : 
+            if limit_len and len(seq)-1-i > limit_len : return [idx, idx+limit_len]
+            else : limit_len = len(seq) - 1 - i; idx = i
         
-#     return [idx, idx+limit_len]
+    return [idx, idx+limit_len]
