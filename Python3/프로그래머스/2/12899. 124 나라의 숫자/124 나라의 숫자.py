fb580a8de1afb723bd339403c7805398d6cc7867
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-16 15:16:42 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ Python3/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,8 +1,33 @@
 def solution(n):
-    answer = ''
-    while n > 0 :
+    ans = ''
+    while n > 0:
         n -= 1
-        answer += '124'[n%3]
-        n //= 3
+        n, r = n//3, n%3
+        print(n, r)
+        ans += str(r+1)
     
-    return answer[::-1]
+    return ans.replace('3','4')[::-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n):
+#     answer = ''
+#     while n > 0 :
+#         n -= 1
+#         answer += '124'[n%3]
+#         n //= 3
+    
+#     return answer[::-1]
