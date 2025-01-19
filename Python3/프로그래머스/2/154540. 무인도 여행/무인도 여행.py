59d5a4f01650d454221570cac001c59403a56cc7
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-19 13:14:53 +0900

    [level 2] Title: 무인도 여행, Time: 1656.21 ms, Memory: 10.7 MB -BaekjoonHub

--- Python3/프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ Python3/프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,32 +1,91 @@
 from collections import deque
 def solution(maps):
-    # maps.insert(0,'X'*len(maps[0]))
-    # maps.append('X'*len(maps[0]))
-    # maps = ['X'+m+'X' for m in maps]
-    directions = [(-1,0),(1,0),(0,-1),(0,1)] # 상하좌우
+    answer = []
     rows, cols = len(maps), len(maps[0])
-    grid = [list(row) for row in maps]
+    ways = [(1,0), (0,1), (0,-1), (-1,0)]
+    maps = [[i for i in m] for m in maps]
     
-    def bfs(x, y):
-        queue = deque([(x, y)])
-        score = int(grid[x][y])
-        grid[x][y] = 'X'
+    def dfs(start):
+        queue = deque([start])
+        x,y,v = start
+        first = int(maps[i][j])
+        maps[x][y] = 'X'
+        s = 0
         
         while queue:
-            x, y = queue.popleft()
-            for dx, dy in directions:
+            x,y,v = queue.popleft()
+            v = int(v)
+            for dx, dy in ways:
                 nx, ny = x+dx, y+dy
-                if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]!='X':
-                    queue.append((nx, ny))
-                    score += int(grid[nx][ny])
-                    grid[nx][ny] = 'X'
+                if 0<=nx<rows and 0<=ny<cols and maps[nx][ny]!='X':
+                    s += int(maps[nx][ny])
+                    queue.append((nx,ny,v+int(maps[nx][ny])))
+                    maps[nx][ny] = 'X'
+        answer.append(s+first)
+    
+    
+    while (rows*cols) != sum(m.count('X') for m in maps):
+        b = False
+        for i in range(rows):
+            for j in range(cols):
+                if maps[i][j]!='X': start = (i,j,0); b = True; break
+            if b : break
+
+        if b : dfs(start)
+    
+    
+    answer.sort()
+    return answer if answer else [-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+# def solution(maps):
+#     # maps.insert(0,'X'*len(maps[0]))
+#     # maps.append('X'*len(maps[0]))
+#     # maps = ['X'+m+'X' for m in maps]
+#     directions = [(-1,0),(1,0),(0,-1),(0,1)] # 상하좌우
+#     rows, cols = len(maps), len(maps[0])
+#     grid = [list(row) for row in maps]
+    
+#     def bfs(x, y):
+#         queue = deque([(x, y)])
+#         score = int(grid[x][y])
+#         grid[x][y] = 'X'
         
-        return score
+#         while queue:
+#             x, y = queue.popleft()
+#             for dx, dy in directions:
+#                 nx, ny = x+dx, y+dy
+#                 if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]!='X':
+#                     queue.append((nx, ny))
+#                     score += int(grid[nx][ny])
+#                     grid[nx][ny] = 'X'
+        
+#         return score
     
-    answer = []
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] != 'X': answer.append(bfs(i,j))
+#     answer = []
+#     for i in range(rows):
+#         for j in range(cols):
+#             if grid[i][j] != 'X': answer.append(bfs(i,j))
     
     
-    return sorted(answer) if answer else [-1]
+#     return sorted(answer) if answer else [-1]
