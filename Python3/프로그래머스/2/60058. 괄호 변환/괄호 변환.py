b7c5d15daa70b545dd679f87e857628cf6d8849a
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-19 14:55:12 +0900

    [level 2] Title: 괄호 변환, Time: 0.80 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ Python3/프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,34 +1,77 @@
-from collections import deque
-def is_correct(u):
-    queue = deque(u)
-    stack = []
-    while queue:
-        v = queue.popleft()
-        if not stack : stack.append(v)
-        elif stack[-1]=='(' and v==')': stack.pop()
-        else : stack.append(v)
-    return not stack
-
 def solution(p):
-    if not p : return '' # 1번
-    answer = ''
     u, v = 0, 0
+    if not p : return ''
     
-    for i in range(2,len(p)+1,2): # 2번
+    def iscorrect(u):
+        stack = []
+        for i in u:
+            if stack and stack[-1]=='(' and i==')': stack.pop(); continue
+            stack.append(i)
+        return False if stack else True
+    
+    for i in range(2,len(p)+1,2):
         u = p[:i]
-        if u.count('(') == u.count(')'):
-            v = p[i:]
-            break
-            
-    if is_correct(u): return u + solution(v) # 3번
-    else: answer = '('+ solution(v) + ')' + ''.join(['(' if i==')' else ')' for i in u[1:-1]]) # 4번
+        if u.count('(') == u.count(')'): v = p[i:]; break
         
-    return answer
+    if iscorrect(u): return u + solution(v) # 3
+    else : # 4
+        new = '(' + solution(v) + ')'
+        return new + (''.join(['(' if i==')' else ')' for i in u[1:-1]]))
+    
 
 
-# 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
-#   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
-#   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
-#   4-3. ')'를 다시 붙입니다. 
-#   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
-#   4-5. 생성된 문자열을 반환합니다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+# def is_correct(u):
+#     queue = deque(u)
+#     stack = []
+#     while queue:
+#         v = queue.popleft()
+#         if not stack : stack.append(v)
+#         elif stack[-1]=='(' and v==')': stack.pop()
+#         else : stack.append(v)
+#     return not stack
+
+# def solution(p):
+#     if not p : return '' # 1번
+#     answer = ''
+#     u, v = 0, 0
+    
+#     for i in range(2,len(p)+1,2): # 2번
+#         u = p[:i]
+#         if u.count('(') == u.count(')'):
+#             v = p[i:]
+#             break
+            
+#     if is_correct(u): return u + solution(v) # 3번
+#     else: answer = '('+ solution(v) + ')' + ''.join(['(' if i==')' else ')' for i in u[1:-1]]) # 4번
+        
+#     return answer
+
+
+# # 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+# #   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+# #   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+# #   4-3. ')'를 다시 붙입니다. 
+# #   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+# #   4-5. 생성된 문자열을 반환합니다.
