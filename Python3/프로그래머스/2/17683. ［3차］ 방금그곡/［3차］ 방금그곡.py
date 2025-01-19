736120051e5591d2e4329ec2b09bd2cf797cea8a
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-19 12:30:15 +0900

    [level 2] Title: [3차] 방금그곡, Time: 5.21 ms, Memory: 10.8 MB -BaekjoonHub

--- Python3/프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ Python3/프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,18 +1,54 @@
 from datetime import datetime as dt
-def solution(m, musicinfos): # 라디오 재생 긴 것, 먼저 입력된 음악.
-    candidates = []
-    def convert_music(music):
-        return music.replace('C#','c').replace('D#','d').replace('F#','f').replace('G#','g').replace('A#','a').replace('B#','b')
+def solution(m, musicinfos):
+    ans_time, answer = -1, ''
+    m = m.replace('C#','c').replace('D#','d').replace('F#','f').replace('G#','g').replace('A#','a').replace('B#','b')
     
-    m = convert_music(m)
+    for music in musicinfos:
+        t1, t2, title, content = music.split(',')
+        content = content.replace('C#','c').replace('D#','d').replace('F#','f').replace('G#','g').replace('A#','a').replace('B#','b')
+        diff = int((dt.strptime(t2,'%H:%M')-dt.strptime(t1,'%H:%M')).total_seconds()//60)
+        content *= (diff//len(content)) + 1
+        content = content[:diff]
+        if content.find(m) != -1 and diff > ans_time : answer = title; ans_time = diff
+        
     
-    for info in musicinfos:
-        t1, t2, title, music = info.split(',')
-        play_time = int((dt.strptime(t2,'%H:%M')-dt.strptime(t1,'%H:%M')).total_seconds()//60)
-        music = convert_music(music)
+    return answer if answer else '(None)'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from datetime import datetime as dt
+# def solution(m, musicinfos): # 라디오 재생 긴 것, 먼저 입력된 음악.
+#     candidates = []
+#     def convert_music(music):
+#         return music.replace('C#','c').replace('D#','d').replace('F#','f').replace('G#','g').replace('A#','a').replace('B#','b')
+    
+#     m = convert_music(m)
+    
+#     for info in musicinfos:
+#         t1, t2, title, music = info.split(',')
+#         play_time = int((dt.strptime(t2,'%H:%M')-dt.strptime(t1,'%H:%M')).total_seconds()//60)
+#         music = convert_music(music)
         
-        full_music = (music*((play_time//len(music))+1))[:play_time]
+#         full_music = (music*((play_time//len(music))+1))[:play_time]
         
-        if m in full_music: candidates.append((play_time, title))
+#         if m in full_music: candidates.append((play_time, title))
             
-    return sorted(candidates, key = lambda x : (-x[0]))[0][1] if candidates else "(None)"
+#     return sorted(candidates, key = lambda x : (-x[0]))[0][1] if candidates else "(None)"
