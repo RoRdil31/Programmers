75f3e939783823fff60807dd16b2c02540fcbcbf
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-19 14:00:56 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 9.98 MB -BaekjoonHub

--- Python3/프로그래머스/2/12936. 줄 서는 방법/README.md
+++ Python3/프로그래머스/2/12936. 줄 서는 방법/README.md
@@ -1,10 +1,10 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 9.98 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 12:57:47
+2025년 01월 19일 14:00:55
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ Python3/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,20 +1,50 @@
-import math
+from math import factorial as fact
 def solution(n, k):
     answer = []
-    p = [i for i in range(1,n+1)]
+    p = list(range(1,n+1))
     
     k -= 1
     while p:
-        fact = math.factorial(len(p)-1)
-        answer.append(p.pop( k//fact )) 
-        k %= fact
-        
+        f = fact(len(p)-1) # 2
+        answer.append( p.pop( k//f ) ) # 2 (append(3))
+        k = k%f
+    
     return answer
 
-# [1]
-# [1,2]             [2,1]
-# [1,2,3] [1,3,2]   [2,1,3] [2,3,1]   [3,1,2] [3,2,1]
+# 4, 7, [2, 1, 3, 4]
 
-# n=4) (24) k=7                     k=6 [2,3,4]
-# [1,2,3,4] [1,2,4,3] [1,3,2,4] [1,3,4,2] [1,4,2,3] [1,4,3,2]
-# [2,1,3,4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import math
+# def solution(n, k):
+#     answer = []
+#     p = [i for i in range(1,n+1)]
+    
+#     k -= 1
+#     while p: 
+#         fact = math.factorial(len(p)-1)
+#         answer.append(p.pop( k//fact )) 
+#         k %= fact
+        
+#     return answer
+
+# # [1]
+# # [1,2]             [2,1]
+# # [1,2,3] [1,3,2]   [2,1,3] [2,3,1]   [3,1,2] [3,2,1]
+
+# # n=4) (24) k=7                     k=6 [2,3,4]
+# # [1,2,3,4] [1,2,4,3] [1,3,2,4] [1,3,4,2] [1,4,2,3] [1,4,3,2]
+# # [2,1,3,4]
