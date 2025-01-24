2cd4d7b86d97cae2f8a1cbaf216a89212b762f5e
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-01-24 10:56:06 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,11 +1,28 @@
 SELECT CART_ID
-    FROM (SELECT CART_ID
-            FROM CART_PRODUCTS
-            GROUP BY CART_ID, NAME
-            HAVING NAME REGEXP 'Milk|Yogurt') AS S
+    FROM CART_PRODUCTS
+    WHERE NAME REGEXP 'Milk|Yogurt'
     GROUP BY CART_ID
-      HAVING COUNT(CART_ID) = 2
-# SELECT CART_ID, NAME
-#                 FROM CART_PRODUCTS
-#                 GROUP BY CART_ID, NAME
-#                   HAVING NAME REGEXP 'Milk|Yogurt' 
+      HAVING COUNT(DISTINCT NAME) >= 2
+    ORDER BY 1;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT CART_ID
+#     FROM (SELECT CART_ID
+#             FROM CART_PRODUCTS
+#             GROUP BY CART_ID, NAME
+#             HAVING NAME REGEXP 'Milk|Yogurt') AS S
+#     GROUP BY CART_ID
+#       HAVING COUNT(CART_ID) = 2
