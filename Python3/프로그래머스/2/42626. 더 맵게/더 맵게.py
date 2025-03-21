a51355b4cebad40b60e13870953bf1353af21c09
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-03-21 16:05:19 +0900

    [level 2] Title: 더 맵게, Time: 1920.21 ms, Memory: 50.9 MB -BaekjoonHub

--- Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,15 @@
-import heapq
-def solution(sco, K):
+import heapq as hq
+def solution(scov, K):
     cnt = 0
-    heapq.heapify(sco)
-    while len(sco) >= 2 and sco[0] < K:
-        m1 = heapq.heappop(sco)
-        m2 = heapq.heappop(sco)
-        heapq.heappush(sco, m1+m2*2)
+    hq.heapify(scov)
+    
+    while len(scov) > 1 and scov[0] < K:
+        m1 = hq.heappop(scov)
+        m2 = hq.heappop(scov)
+        hq.heappush(scov, m1 + (m2*2))
         cnt += 1
     
-    return cnt if sco[0]>=K else -1
-
-
-
+    return cnt if scov[0] >= K else -1
 
 
 
@@ -30,15 +28,46 @@ def solution(sco, K):
 
 
 # import heapq
-# def solution(scov, K):
+# def solution(sco, K):
 #     cnt = 0
-#     heapq.heapify(scov)
-#     while len(scov) > 1:
-#         m1 = heapq.heappop(scov)
-#         m2 = heapq.heappop(scov)
-#         if (m1 >= K) : return cnt
-#         new = m1 + m2*2
-#         heapq.heappush(scov, new)
+#     heapq.heapify(sco)
+#     while len(sco) >= 2 and sco[0] < K:
+#         m1 = heapq.heappop(sco)
+#         m2 = heapq.heappop(sco)
+#         heapq.heappush(sco, m1+m2*2)
 #         cnt += 1
+    
+#     return cnt if sco[0]>=K else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import heapq
+# # def solution(scov, K):
+# #     cnt = 0
+# #     heapq.heapify(scov)
+# #     while len(scov) > 1:
+# #         m1 = heapq.heappop(scov)
+# #         m2 = heapq.heappop(scov)
+# #         if (m1 >= K) : return cnt
+# #         new = m1 + m2*2
+# #         heapq.heappush(scov, new)
+# #         cnt += 1
         
-#     return cnt if scov[0] >= K else -1
+# #     return cnt if scov[0] >= K else -1
