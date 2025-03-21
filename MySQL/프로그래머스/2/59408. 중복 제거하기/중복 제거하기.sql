6d7482eac6cbf0206cf002656e0d9a4891529c20
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-03-21 15:35:12 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ MySQL/프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,4 +1,18 @@
--- 코드를 입력하세요
-SELECT COUNT(DISTINCT NAME) AS 'count' 
-    FROM ANIMAL_INS
-    WHERE NAME IS NOT NULL;
+select count(distinct name) as 'count'
+    from animal_ins
+
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# SELECT COUNT(DISTINCT NAME) AS 'count' 
+#     FROM ANIMAL_INS
+#     WHERE NAME IS NOT NULL;
