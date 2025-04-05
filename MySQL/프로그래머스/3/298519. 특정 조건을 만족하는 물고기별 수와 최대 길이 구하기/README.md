bb1ba01568228afb224d35a6b8bf69942cca7fc5
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 13:44:30 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/README.md
+++ MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/README.md
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 14일 18:10:52
+2025년 04월 05일 13:44:24
 
 ### 문제 설명
 
@@ -119,9 +119,12 @@
 <th>FISH_TYPE</th>
 </tr>
 </thead>
-        <tbody></tbody>
+        <tbody><tr>
+<td>3</td>
+<td>50</td>
+<td>0</td>
+</tr>
+</tbody>
       </table>
-<p>| 3 | 50 | 0 |</p>
-
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ MySQL/프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,11 +1,11 @@
-SELECT COUNT(*) FISH_COUNT, MAX(LENGTH) MAX_LENGTH, FISH_TYPE
-    FROM FISH_INFO
-    GROUP BY 3
-      HAVING AVG(CASE
-                WHEN LENGTH IS NULL THEN 10
-                ELSE LENGTH
-             END) >= 33
-    ORDER BY 3
+SELECT COUNT(ID) AS FISH_COUNT
+        , MAX(LENGTH) AS MAX_LENGTH
+        , FISH_TYPE
+FROM FISH_INFO
+GROUP BY FISH_TYPE
+  HAVING AVG(IFNULL(LENGTH, 10)) >= 33
+
+ORDER BY 3
 
 
 
@@ -22,16 +22,40 @@ SELECT COUNT(*) FISH_COUNT, MAX(LENGTH) MAX_LENGTH, FISH_TYPE
 
 
 
-
-
-
-# SELECT COUNT(*)    AS FISH_COUNT, 
-#        MAX(LENGTH) AS MAX_LENGTH, 
-#        FISH_TYPE
+# SELECT COUNT(*) FISH_COUNT, MAX(LENGTH) MAX_LENGTH, FISH_TYPE
 #     FROM FISH_INFO
-#     GROUP BY FISH_TYPE
+#     GROUP BY 3
 #       HAVING AVG(CASE
-#                     WHEN LENGTH > 10 THEN LENGTH
-#                     ELSE 10
-#                  END) >= 33
-#     ORDER BY FISH_TYPE
+#                 WHEN LENGTH IS NULL THEN 10
+#                 ELSE LENGTH
+#              END) >= 33
+#     ORDER BY 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # SELECT COUNT(*)    AS FISH_COUNT, 
+# #        MAX(LENGTH) AS MAX_LENGTH, 
+# #        FISH_TYPE
+# #     FROM FISH_INFO
+# #     GROUP BY FISH_TYPE
+# #       HAVING AVG(CASE
+# #                     WHEN LENGTH > 10 THEN LENGTH
+# #                     ELSE 10
+# #                  END) >= 33
+# #     ORDER BY FISH_TYPE
