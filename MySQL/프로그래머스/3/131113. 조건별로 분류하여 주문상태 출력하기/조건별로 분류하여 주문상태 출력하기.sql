afe97f5332b990b2f7e5fb5acb87199220793481
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 11:55:49 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ MySQL/프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -1,6 +1,29 @@
-SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE,'%Y-%m-%d'), CASE
-    WHEN DATEDIFF('2022-05-01', OUT_DATE) >= 0 THEN '출고완료'
-    WHEN DATEDIFF('2022-05-01', OUT_DATE) < 0 THEN '출고대기'
-    ELSE '출고미정' END AS 출고여부
-    FROM FOOD_ORDER
-    ORDER BY ORDER_ID 
+SELECT order_id
+        , product_id
+        , DATE_FORMAT(out_date, '%Y-%m-%d') AS 'out_date'
+        , CASE
+            WHEN out_date <= '2022-05-02' THEN '출고완료'
+            WHEN out_date IS NULL THEN '출고미정'
+            ELSE '출고대기'
+        END AS '출고여부'
+    FROM food_order
+    order by 1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE,'%Y-%m-%d'), CASE
+#     WHEN DATEDIFF('2022-05-01', OUT_DATE) >= 0 THEN '출고완료'
+#     WHEN DATEDIFF('2022-05-01', OUT_DATE) < 0 THEN '출고대기'
+#     ELSE '출고미정' END AS 출고여부
+#     FROM FOOD_ORDER
+#     ORDER BY ORDER_ID
