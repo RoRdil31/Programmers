000d8698032bbfd2a94375d255aae551846fd7b2
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 14:12:00 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ MySQL/프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -1,7 +1,53 @@
-SELECT P.ITEM_ID, ITEM_NAME, RARITY
-    FROM ITEM_INFO P
-    LEFT
-    JOIN ITEM_TREE C
-      ON P.ITEM_ID = C.PARENT_ITEM_ID
-    WHERE PARENT_ITEM_ID IS NULL
-    ORDER BY 1 DESC
+SELECT I.ITEM_ID, I.ITEM_NAME, I.RARITY
+FROM ITEM_INFO I
+LEFT JOIN ITEM_TREE T
+  ON I.ITEM_ID = T.PARENT_ITEM_ID
+WHERE T.ITEM_ID IS NULL
+
+ORDER BY 1 DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select 
+#         i.item_id
+#         , item_name
+#         , rarity
+#     from item_info i
+#     left join item_tree t
+#            on i.item_id = t.parent_item_id
+#     where t.parent_item_id is null
+#     order by 1 desc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # SELECT P.ITEM_ID, ITEM_NAME, RARITY
+# #     FROM ITEM_INFO P
+# #     LEFT
+# #     JOIN ITEM_TREE C
+# #       ON P.ITEM_ID = C.PARENT_ITEM_ID
+# #     WHERE PARENT_ITEM_ID IS NULL
+# #     ORDER BY 1 DESC
