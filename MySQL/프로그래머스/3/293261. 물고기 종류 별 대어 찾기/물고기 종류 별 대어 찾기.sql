814c89df903ca1e9a7f04e72685d8e82f2420ac3
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 14:36:02 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,9 +1,78 @@
 SELECT ID, FISH_NAME, LENGTH
-    FROM (SELECT *, MAX(LENGTH) OVER(PARTITION BY FISH_TYPE) AS MAX_LEN
-            FROM FISH_INFO) I
-    JOIN FISH_NAME_INFO N ON I.FISH_TYPE = N.FISH_TYPE
-    WHERE LENGTH = MAX_LEN
-    ORDER BY 1
+FROM (SELECT ID, FISH_TYPE, LENGTH, MAX(LENGTH) OVER(PARTITION BY FISH_TYPE) AS MAX_LENGTH
+        FROM FISH_INFO
+     ) I
+JOIN FISH_NAME_INFO N
+  ON I.FISH_TYPE = N.FISH_TYPE
+WHERE LENGTH = MAX_LENGTH 
+ORDER BY 1
+
+
+
+
+
+
+
+
+
+
+
+# SELECT id
+#         , fish_name
+#         , length
+#     FROM (select *, 
+#             max(length) over(partition by fish_type) as max_length
+#             from fish_info) i
+#     join fish_name_info n on i.fish_type = n.fish_type
+#     where length = max_length
+#     order by 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # select 
+# #         id
+# #         , fish_name
+# #         , length
+# #     from (select *, max(length) over(partition by fish_type) as max_len
+# #             from fish_info) s
+# #     join fish_name_info n
+# #       on s.fish_type = n.fish_type
+# #     where length = max_len
+    
+# #     order by 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # # SELECT ID, FISH_NAME, LENGTH
+# # # #     FROM (SELECT *, MAX(LENGTH) OVER(PARTITION BY FISH_TYPE) AS MAX_LEN
+# # # #             FROM FISH_INFO) I
+# # # #     JOIN FISH_NAME_INFO N ON I.FISH_TYPE = N.FISH_TYPE
+# # # #     WHERE LENGTH = MAX_LEN
+# # # #     ORDER BY 1
 
 
 
@@ -24,14 +93,12 @@ SELECT ID, FISH_NAME, LENGTH
 
 
 
-
-
-# SELECT ID, FISH_NAME, LENGTH
-#     FROM FISH_INFO I
-#     JOIN FISH_NAME_INFO N
-#       ON I.FISH_TYPE = N.FISH_TYPE
-#     WHERE I.LENGTH = (
-#         SELECT MAX(I2.LENGTH)
-#         FROM FISH_INFO I2
-#         WHERE I.FISH_TYPE = I2.FISH_TYPE)
-#     ORDER BY 1
+# # # # SELECT ID, FISH_NAME, LENGTH
+# # # #     FROM FISH_INFO I
+# # # #     JOIN FISH_NAME_INFO N
+# # # #       ON I.FISH_TYPE = N.FISH_TYPE
+# # # #     WHERE I.LENGTH = (
+# # # #         SELECT MAX(I2.LENGTH)
+# # # #         FROM FISH_INFO I2
+# # # #         WHERE I.FISH_TYPE = I2.FISH_TYPE)
+# # # #     ORDER BY 1
