d6c7bcd3fb601cbf3e78a300dd82a044d03986df
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 13:38:14 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,6 +1,31 @@
-SELECT *
-    FROM PLACES
-    WHERE HOST_ID IN (SELECT HOST_ID 
-                        FROM PLACES 
-                        GROUP BY HOST_ID HAVING COUNT(ID) >= 2)
-    ORDER BY ID
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (SELECT HOST_ID FROM PLACES
+                GROUP BY HOST_ID HAVING COUNT(ID) > 1)
+ORDER BY 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT *
+#     FROM PLACES
+#     WHERE HOST_ID IN (SELECT HOST_ID 
+#                         FROM PLACES 
+#                         GROUP BY HOST_ID HAVING COUNT(ID) >= 2)
+#     ORDER BY ID
