3c28aef8334f0e93c361e0078f9b986d44089f66
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 11:55:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,27 @@
-SELECT NAME, DATETIME
-    FROM ANIMAL_INS
-    WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_OUTS)
-    ORDER BY DATETIME
-    LIMIT 3
+select 
+    i.name
+    , i.datetime
+    from animal_ins i left join animal_outs o on i.animal_id = o.animal_id
+    where o.animal_id IS NULL
+    order by 2
+    limit 3
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT NAME, DATETIME
+#     FROM ANIMAL_INS
+#     WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_OUTS)
+#     ORDER BY DATETIME
+#     LIMIT 3
