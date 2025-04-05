4c38acfeae9503ce0540b1ad3a280234233595b1
Minji <117567368+RoRdil31@users.noreply.github.com>
2025-04-05 14:56:28 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,16 +1,14 @@
-SELECT ID, 
-        CASE
-            WHEN RANKS <= 0.25 THEN 'CRITICAL'
-            WHEN RANKS <= 0.5 THEN 'HIGH'
-            WHEN RANKS <= 0.75 THEN 'MEDIUM'
+SELECT ID
+        ,CASE
+            WHEN per <= 0.25 THEN 'CRITICAL'
+            WHEN per <= 0.50 THEN 'HIGH'
+            WHEN per <= 0.75 THEN 'MEDIUM'
             ELSE 'LOW'
-        END AS COLONY_NAME
-    FROM (SELECT ID, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY) AS RANKS
-            FROM ECOLI_DATA) AS SUB 
-    ORDER BY 1
+        END AS 'COLONY_NAME'
+FROM (SELECT ID, ROW_NUMBER() OVER(ORDER BY SIZE_OF_COLONY DESC) / COUNT(ID) OVER() AS 'per'
+        FROM ECOLI_DATA) S
 
-# SELECT ID,SIZE_OF_COLONY, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY)
-#             FROM ECOLI_DATA
+ORDER BY 1
 
 
 
@@ -28,15 +26,47 @@ SELECT ID,
 
 
 
-# # SELECT ID, 
-# #         CASE 
-# #             WHEN S.RANKS/CNTS <= 0.25 THEN 'CRITICAL'
-# #             WHEN S.RANKS/CNTS <= 0.5 THEN 'HIGH'
-# #             WHEN S.RANKS/CNTS <= 0.75 THEN 'MEDIUM'
-# #             ELSE 'LOW'
-# #         END AS COLONY_NAME
-# #     FROM (SELECT *, 
-# #             RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS RANKS, 
-# #             COUNT(*) OVER() AS CNTS
-# #           FROM ECOLI_DATA) AS S
-# #     ORDER BY 1
+
+
+# SELECT ID, 
+#         CASE
+#             WHEN RANKS <= 0.25 THEN 'CRITICAL'
+#             WHEN RANKS <= 0.5 THEN 'HIGH'
+#             WHEN RANKS <= 0.75 THEN 'MEDIUM'
+#             ELSE 'LOW'
+#         END AS COLONY_NAME
+#     FROM (SELECT ID, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY) AS RANKS
+#             FROM ECOLI_DATA) AS SUB 
+#     ORDER BY 1
+
+# # SELECT ID,SIZE_OF_COLONY, 1-PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY)
+# #             FROM ECOLI_DATA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # SELECT ID, 
+# # #         CASE 
+# # #             WHEN S.RANKS/CNTS <= 0.25 THEN 'CRITICAL'
+# # #             WHEN S.RANKS/CNTS <= 0.5 THEN 'HIGH'
+# # #             WHEN S.RANKS/CNTS <= 0.75 THEN 'MEDIUM'
+# # #             ELSE 'LOW'
+# # #         END AS COLONY_NAME
+# # #     FROM (SELECT *, 
+# # #             RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS RANKS, 
+# # #             COUNT(*) OVER() AS CNTS
+# # #           FROM ECOLI_DATA) AS S
+# # #     ORDER BY 1
